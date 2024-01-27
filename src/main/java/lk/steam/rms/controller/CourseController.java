f19abee33047fc4d38322ca23ba555a13eb26e9f
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-01-27 17:20:39 +0530

    initial commit

---
+++ src/main/java/lk/steam/rms/controller/CourseController.java
@@ -0,0 +1,24 @@
+package lk.steam.rms.controller;
+
+import lk.steam.ims.dao.CourseDAO;
+import lk.steam.ims.entity.Course;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/course")
+public class CourseController {
+
+    @Autowired
+    private CourseDAO courseDAO;
+
+    @GetMapping(value = "/findall",produces = "application/json")
+    public List<Course> findAll(){
+        return courseDAO.findAll();
+    }
+
+}
