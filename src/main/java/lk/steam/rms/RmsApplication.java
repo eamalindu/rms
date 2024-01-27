26fa9d224df058eca8fa28c422237a088ed48c26
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-01-27 16:45:06 +0530

    nav bar edited

--- src/main/java/lk/steam/rms/RmsApplication.java
+++ src/main/java/lk/steam/rms/RmsApplication.java
@@ -2,13 +2,24 @@ package lk.steam.rms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @SpringBootApplication
+@RestController
 public class RmsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RmsApplication.class, args);
 		System.out.println("RMS System is now running!");
+	}
+
+	@RequestMapping(value = "/New-Registration")
+	public ModelAndView imsInquiries(){
+		ModelAndView imsInquiriesView = new ModelAndView();
+		imsInquiriesView.setViewName("newregistration.html");
+		return imsInquiriesView;
 	}
 
 }
