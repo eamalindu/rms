af3ae8039d53617814b016d9b4a034761f131906
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-01-28 18:52:44 +0530

    new getBatchByCourse method added

--- src/main/java/lk/steam/rms/dao/BatchDAO.java
+++ src/main/java/lk/steam/rms/dao/BatchDAO.java
@@ -1,7 +1,16 @@
 package lk.steam.rms.dao;
 
 import lk.steam.rms.entity.Batch;
+import lk.steam.rms.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface BatchDAO extends JpaRepository<Batch,Integer> {
+
+    @Query(value = "SELECT b from Batch b where b.courseID=?1")
+    List<Batch> getBatchByCourse(Course course);
+
+
 }
