01353a2769b2d82ae715bfce53d5fe37c405ef44
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-01-28 18:56:27 +0530

    edited

--- src/main/java/lk/steam/rms/dao/BatchDAO.java
+++ src/main/java/lk/steam/rms/dao/BatchDAO.java
@@ -1,7 +1,6 @@
 package lk.steam.rms.dao;
 
 import lk.steam.rms.entity.Batch;
-import lk.steam.rms.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
