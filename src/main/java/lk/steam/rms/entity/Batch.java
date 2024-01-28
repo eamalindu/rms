a81f4a918197553019435da45ef542263dc5549f
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-01-28 18:45:33 +0530

    entity mapped

--- src/main/java/lk/steam/rms/entity/Batch.java
+++ src/main/java/lk/steam/rms/entity/Batch.java
@@ -1,9 +1,14 @@
 package lk.steam.rms.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "batch")
@@ -18,5 +23,60 @@ public class Batch {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @Column(name = "batchcode")
+    @NotNull
+    private String batchCode;
+
+    @Column(name = "commencedate")
+    @NotNull
+    private LocalDate commenceDate;
+
+    @Column(name = "enddate")
+    @NotNull
+    private LocalDate endDate;
+
+    @Column(name = "seatcount")
+    @NotNull
+    private Integer seatCount;
+
+    @Column(name = "createdby")
+    @NotNull
+    private String createdBy;
+
+    @Column(name = "timestamp")
+    @NotNull
+    private LocalDateTime timestamp;
+
+    @Column(name = "isweekday")
+    @NotNull
+    private Boolean isWeekday;
+
+    @Column(name = "description")
+    @NotNull
+    private String description;
+
+    @Column(name = "totalFee")
+    @NotNull
+    private BigDecimal totalFee;
+
+    @Column(name = "registrationfee")
+    @NotNull
+    private BigDecimal registrationFee;
+
+    @Column(name = "remainingfee")
+    @NotNull
+    private BigDecimal remainingFee;
+
+    @Column(name = "numberofinstallments")
+    @NotNull
+    private Integer numberOfInstallments;
+
+    @ManyToOne
+    @JoinColumn(name = "course_id",referencedColumnName = "id")
+    private Course courseID;
+
+//    private String lectureRoomID;
+//    private String lecturerID;
+//    private String batchStatusID;
 
 }
