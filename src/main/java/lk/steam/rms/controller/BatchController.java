01a6c76b6fd39920407e07ec6d8dfe3d10d502c9
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-01-28 18:48:17 +0530

    service mapped

--- src/main/java/lk/steam/rms/controller/BatchController.java
+++ src/main/java/lk/steam/rms/controller/BatchController.java
@@ -1,5 +1,26 @@
 package lk.steam.rms.controller;
 
+import lk.steam.rms.dao.BatchDAO;
+import lk.steam.rms.entity.Batch;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/Batch")
 public class BatchController {
+
+    @Autowired
+    private BatchDAO batchDAO;
+
+    @GetMapping(value = "/findall",produces = "application/json")
+    public List<Batch> findAll(){
+        return batchDAO.findAll();
+    }
+
+
+
 }
