ac0e9f60e0b3703a2cf878c442e5f6d2a1653345
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-01-29 22:59:32 +0530

    edited

--- src/main/resources/static/resources/js/controllerJS/new-registration.js
+++ src/main/resources/static/resources/js/controllerJS/new-registration.js
@@ -153,24 +153,29 @@ let previous0 = () => {
 
 }
 let previous1 = () => {
-    //footer_1 value should be present in order to execute the following code -> use if
-    step2.classList.remove('custom-step-complete');
-    step3.classList.remove('custom-step-complete');
-    step4.classList.remove('custom-step-complete');
-    step5.classList.remove('custom-step-complete');
+    if(registration.courseID!==undefined) {
+        //footer_1 value should be present in order to execute the following code -> use if
+        step2.classList.remove('custom-step-complete');
+        step3.classList.remove('custom-step-complete');
+        step4.classList.remove('custom-step-complete');
+        step5.classList.remove('custom-step-complete');
 
 
+        footer_1.classList.remove('show');
+        footer_2.classList.add('show');
+        footer_3.classList.remove('show');
+        footer_4.classList.remove('show');
+        footer_5.classList.remove('show');
 
-    footer_1.classList.remove('show');
-    footer_2.classList.add('show');
-    footer_3.classList.remove('show');
-    footer_4.classList.remove('show');
-    footer_5.classList.remove('show');
+        document.querySelector('#btn-batch .step-number span').innerText = '2';
+        document.querySelector('#btn-payment-str .step-number span').innerText = '3';
+        document.querySelector('#btn-student .step-number span').innerText = '4';
+        document.querySelector('#btn-add-payment .step-number span').innerText = '5';
+    }
+    else{
+        showCustomModal("Please Select a Course !","warning");
 
-    document.querySelector('#btn-batch .step-number span').innerText = '2';
-    document.querySelector('#btn-payment-str .step-number span').innerText = '3';
-    document.querySelector('#btn-student .step-number span').innerText = '4';
-    document.querySelector('#btn-add-payment .step-number span').innerText = '5';
+    }
 
 
 
