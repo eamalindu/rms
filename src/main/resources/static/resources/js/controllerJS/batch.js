16f5bdf817c4cb01f7cf4a8584fe6f13fced9e19
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-01-30 18:07:37 +0530

    table loaded

--- src/main/resources/static/resources/js/controllerJS/batch.js
+++ src/main/resources/static/resources/js/controllerJS/batch.js
@@ -1,3 +1,42 @@
 window.addEventListener("load",()=>{
-
+    //refresh the batch table
+    refreshBatchTable();
 });
+
+
+const refreshBatchTable =()=>{
+
+    //getting current batches from the database using ajaxGetRequest function and assign the response to the variable batches
+    batches = ajaxGetRequest("/Batch/findall");
+    //creating a display property list for the batches
+    displayPropertyListForBatches = [
+        {property:getCourseName,dataType:'function'},
+        {property:'batchCode',dataType:'text'},
+        {property:'commenceDate',dataType:'text'},
+        {property:'endDate',dataType:'text'},
+        {property:getWeekDay,dataType:'function'},
+        {property:'seatCount',dataType:'text'},
+        {property:'description',dataType:'text'},
+        {property:'createdBy',dataType:'text'},
+    ];
+
+    fillDataIntoTable(tblBatch,batches,displayPropertyListForBatches,rowView,'')
+}
+
+const getCourseName = (ob)=>{
+    return ob.courseID.name;
+}
+
+const getWeekDay = (ob)=>{
+    if(ob.isWeekday){
+        return "WeekDay";
+    }
+    else{
+        return "Weekend";
+    }
+
+}
+
+const rowView = ()=>{
+
+}
