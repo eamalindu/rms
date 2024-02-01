5beff65f4737920c88e7e1e9712e655da40506e6
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-02-01 09:01:16 +0530

    BatchStatus Mapped

---
+++ src/main/java/lk/steam/rms/entity/BatchStatus.java
@@ -0,0 +1,25 @@
+package lk.steam.rms.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "batchstatus")
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BatchStatus {
+
+    @Id
+    @Column(name = "id",unique = true)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name")
+    @NotNull
+    private String name;
+}
