22cdd9345f507665be7bdaf90d5a704268b83293
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-02-01 09:06:09 +0530

    findAll service mapped

--- src/main/java/lk/steam/rms/controller/BatchStatusController.java
+++ src/main/java/lk/steam/rms/controller/BatchStatusController.java
@@ -1,9 +1,23 @@
 package lk.steam.rms.controller;
 
+import lk.steam.rms.dao.BatchStatusDAO;
+import lk.steam.rms.entity.BatchStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/BatchStatus")
 public class BatchStatusController {
+
+    @Autowired
+    private BatchStatusDAO batchStatusDAO;
+
+    @GetMapping(value = "/findall")
+    public List<BatchStatus> findAll(){
+        return batchStatusDAO.findAll();
+    }
 }
