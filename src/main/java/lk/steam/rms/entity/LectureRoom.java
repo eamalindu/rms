5071b08122cc8456013ce2cb9cdd0cd756414a36
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-02-01 21:43:09 +0530

    mapping started

--- src/main/java/lk/steam/rms/entity/LectureRoom.java
+++ src/main/java/lk/steam/rms/entity/LectureRoom.java
@@ -1,4 +1,22 @@
 package lk.steam.rms.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "lectureroom")
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class LectureRoom {
+
+    @Id
+    @Column(name = "id",unique = true)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+
 }
