e45252f06cdd94af7e44652324edaa2c0341b213
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-02-01 09:04:43 +0530

    BatchStatusController implemented

---
+++ src/main/java/lk/steam/rms/controller/BatchStatusController.java
@@ -0,0 +1,9 @@
+package lk.steam.rms.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/BatchStatus")
+public class BatchStatusController {
+}
