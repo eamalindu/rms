418ae7b3e782658c75d2043379dd59855e6e133f
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-02-08 17:31:25 +0530

    dao mapped

---
+++ src/main/java/lk/steam/rms/dao/DayDAO.java
@@ -0,0 +1,7 @@
+package lk.steam.rms.dao;
+
+import lk.steam.rms.entity.Day;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DayDAO extends JpaRepository<Day,Integer> {
+}
