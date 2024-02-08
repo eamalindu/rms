2cad38834278237d4dfe42bfcb09cf00d0df83d4
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-02-08 17:30:38 +0530

    entity mapped

---
+++ src/main/java/lk/steam/rms/entity/Day.java
@@ -0,0 +1,24 @@
+package lk.steam.rms.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "day")
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Day {
+    @Id
+    @Column(name = "id",unique = true)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name")
+    @NotNull
+    private String name;
+}
