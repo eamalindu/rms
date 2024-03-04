a49c687471afad7694d22a9dbc81e1610cb41d1e
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-03-04 15:13:28 +0530

    new service added

--- src/main/java/lk/steam/rms/controller/LectureRoomController.java
+++ src/main/java/lk/steam/rms/controller/LectureRoomController.java
@@ -1,4 +1,24 @@
 package lk.steam.rms.controller;
 
+import lk.steam.rms.dao.LectureRoomDAO;
+import lk.steam.rms.dao.PaymentPlanDAO;
+import lk.steam.rms.entity.LectureRoom;
+import lk.steam.rms.entity.PaymentPlan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/LectureRoom")
 public class LectureRoomController {
+    @Autowired
+    private LectureRoomDAO lectureRoomDAO;
+
+    @GetMapping(value = "/findall",produces = "application/json")
+    public List<LectureRoom> findAll(){
+        return lectureRoomDAO.findAll();
+    }
 }
