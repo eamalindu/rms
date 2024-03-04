a648da1effcf18f974c2495cc88e26eeec88bc7a
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-03-04 11:01:07 +0530

    getPaymentPlanByCourseID added

--- src/main/java/lk/steam/rms/dao/PaymentPlanDAO.java
+++ src/main/java/lk/steam/rms/dao/PaymentPlanDAO.java
@@ -2,7 +2,13 @@ package lk.steam.rms.dao;
 
 import lk.steam.rms.entity.PaymentPlan;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PaymentPlanDAO extends JpaRepository<PaymentPlan,Integer> {
 
+    //get all the active payment plans by course id
+    @Query("SELECT p from PaymentPlan p where p.courseID.id=?1 and p.status=true")
+    List<PaymentPlan> getPaymentPlanByCourseID(Integer courseId);
 }
