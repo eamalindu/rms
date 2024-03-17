17fc0c2be294583031f8afd152c9f054d510d747
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-03-17 15:37:32 +0530

    query changed

--- src/main/java/lk/steam/rms/dao/BatchDAO.java
+++ src/main/java/lk/steam/rms/dao/BatchDAO.java
@@ -9,10 +9,10 @@ import java.util.List;
 
 public interface BatchDAO extends JpaRepository<Batch,Integer> {
 
-    @Query("SELECT b FROM Batch b WHERE b.courseID.id =?1 and b.isWeekday = true and (b.batchStatusID.id=1 or b.batchStatusID.id=2)")
+    @Query("SELECT b FROM Batch b WHERE b.courseID.id =?1 and b.isWeekday = true and (b.batchStatusID.id=1 or b.batchStatusID.id=2) and b.commenceDate >= curdate() ")
     List<Batch> getActiveWeekDayBatchesByCourseId(Integer courseId);
 
-    @Query("SELECT b FROM Batch b WHERE b.courseID.id =?1 and b.isWeekday = false and (b.batchStatusID.id=1 or b.batchStatusID.id=2)")
+    @Query("SELECT b FROM Batch b WHERE b.courseID.id =?1 and b.isWeekday = false and (b.batchStatusID.id=1 or b.batchStatusID.id=2) and b.commenceDate >= curdate()")
     List<Batch> getActiveWeekendBatchesByCourseId(Integer courseId);
 
     @Query("SELECT CAST(SUBSTRING_INDEX(b.batchCode, '-', -1) AS INTEGER) + 1 AS nextBatchCode  FROM Batch b where b.courseID.id=?1 ORDER BY b.id DESC limit 1")
