8618eee3005af4d70b00f757c750d464b7e635a0
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-03-21 12:02:33 +0530

    edited

---
+++ src/main/resources/static/resources/js/controllerJS/course.js
@@ -0,0 +1,16 @@
+window.addEventListener("load", () => {
+
+    //reset the course form
+    resetCourseForm();
+    //refresh the course table
+    refreshCourseTable();
+
+});
+
+const refreshCourseTable = ()=>{
+
+}
+
+const resetCourseForm = ()=>{
+    course = {};
+}
