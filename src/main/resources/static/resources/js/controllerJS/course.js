b186bf6d27c0c58b493021505e828c04da3de44f
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-03-21 12:05:33 +0530

    refreshCourseTable edited

--- src/main/resources/static/resources/js/controllerJS/course.js
+++ src/main/resources/static/resources/js/controllerJS/course.js
@@ -8,6 +8,21 @@ window.addEventListener("load", () => {
 });
 
 const refreshCourseTable = ()=>{
+    //getting current batches from the database using ajaxGetRequest function and assign the response to the variable batches
+    courses = ajaxGetRequest("/Course/findall");
+    //creating a display property list for the batches
+    displayPropertyListForCourse = [
+        {property: 'name',dataType: 'text'},
+        {property: 'code',dataType: 'text'},
+        {property: getDuration,dataType: 'function'},
+        {property: 'minimumRequirement',dataType: 'text'},
+        {property: 'lectureHours',dataType: 'text'},
+        {property: getStatus,dataType: 'function'},
+    ]
+
+    fillDataIntoTable(tblCourse, courses, displayPropertyListForCourse, rowView, '');
+
+    $('#tblBatch').DataTable();
 
 }
 
