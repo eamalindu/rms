a0927c63b3cb16c419ea94ddab98e3d3383475f5
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-03-21 11:22:13 +0530

    new service mapped

--- src/main/java/lk/steam/rms/controller/CourseController.java
+++ src/main/java/lk/steam/rms/controller/CourseController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -19,6 +20,13 @@ public class CourseController {
     @GetMapping(value = "/findall",produces = "application/json")
     public List<Course> findAll(){
         return courseDAO.findAll();
+    }
+
+    @GetMapping()
+    public ModelAndView courseUI() {
+        ModelAndView courseView = new ModelAndView();
+        courseView.setViewName("course.html");
+        return courseView;
     }
 
 }
