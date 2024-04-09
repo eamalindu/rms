15be289f2aea6bb881c3c61596a6673989dd0108
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-04-09 16:38:15 +0530

    edited

---
+++ src/main/java/lk/steam/rms/controller/RegistrationStatusController.java
@@ -0,0 +1,25 @@
+package lk.steam.rms.controller;
+
+import lk.steam.rms.dao.BatchStatusDAO;
+import lk.steam.rms.dao.RegistrationDAO;
+import lk.steam.rms.dao.RegistrationStatusDAO;
+import lk.steam.rms.entity.BatchStatus;
+import lk.steam.rms.entity.RegistrationStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/RegistrationStatus")
+public class RegistrationStatusController {
+    @Autowired
+    private RegistrationStatusDAO registrationStatusDAO;
+
+    @GetMapping(value = "/findall")
+    public List<RegistrationStatus> findAll(){
+        return registrationStatusDAO.findAll();
+    }
+}
