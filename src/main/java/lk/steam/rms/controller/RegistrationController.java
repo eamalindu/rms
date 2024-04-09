61ec08741b27c2e35ab932ba877772fbb062117a
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-04-09 16:28:45 +0530

    edited

--- src/main/java/lk/steam/rms/controller/RegistrationController.java
+++ src/main/java/lk/steam/rms/controller/RegistrationController.java
@@ -3,6 +3,7 @@ package lk.steam.rms.controller;
 import lk.steam.rms.dao.RegistrationDAO;
 import lk.steam.rms.dao.RegistrationStatusDAO;
 import lk.steam.rms.dao.StudentDAO;
+import lk.steam.rms.entity.Batch;
 import lk.steam.rms.entity.RegistrationStatus;
 import lk.steam.rms.entity.Registrations;
 import lk.steam.rms.entity.Student;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @RestController
 @RequestMapping("/Registration")
@@ -48,5 +50,10 @@ public class RegistrationController {
         registrationDAO.save(registrations);
         return "OK";
 
+    }
+
+    @GetMapping(value = "/findall", produces = "application/json")
+    public List<Registrations> findAll() {
+        return registrationDAO.findAll();
     }
 }
