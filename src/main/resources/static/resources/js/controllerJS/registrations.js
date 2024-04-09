a69ef7fa5869ceb0eaca9074fd0315acc5a0f72f
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-04-09 15:36:48 +0530

    edited

--- src/main/resources/static/resources/js/controllerJS/registrations.js
+++ src/main/resources/static/resources/js/controllerJS/registrations.js
@@ -4,15 +4,10 @@ window.addEventListener("load", () => {
     resetRegistrationForm();
     //refresh the registration table
     refreshRegistrationTable();
-});
 
-//creating a function to refresh the registrations table when ever needed
-const refreshRegistrationTable = ()=>{
-
-}
-
-//creating a function to reset the registrations form when ever needed
-const resetRegistrationForm=()=>{
+    //refresh/refill search filters
+    courses =  ajaxGetRequest("/Course/findall");
+    fillSelectOptions(registrationSearchCourse, ' ', courses, 'name');
 
     //initialize the 3rd party libraries (chosen)
     $('.chosen-registration-search').chosen({width: '190px'});
@@ -23,4 +18,23 @@ const resetRegistrationForm=()=>{
         }
     });
 
+});
+
+//creating a function to refresh the registrations table when ever needed
+const refreshRegistrationTable = ()=>{
+
 }
+
+//creating a function to reset the registrations form when ever needed
+const resetRegistrationForm=()=>{
+
+
+
+}
+
+//reset chosen select using jquery
+$('#btn-reset').on('click', function () {
+    setTimeout(function () {
+        $('.chosen-registration-search').trigger('chosen:updated');
+    }, 0);
+});
