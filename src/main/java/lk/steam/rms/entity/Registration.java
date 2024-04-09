b25388dda302bda082c970a3654fdc93c03b654b
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-04-09 12:33:41 +0530

    properties added

--- src/main/java/lk/steam/rms/entity/Registration.java
+++ src/main/java/lk/steam/rms/entity/Registration.java
@@ -1,10 +1,14 @@
 package lk.steam.rms.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "registration")
@@ -13,4 +17,15 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Registration {
+
+    @Id
+    private Integer id;
+    private String registrationNumber;
+    private String addedBy;
+    private LocalDateTime timestamp;
+    private Boolean isFullPayment;
+    private String commissionPaidTo;
+    private BigDecimal oneTimePaymentAmount;
+
+
 }
