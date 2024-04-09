66d54ccb8179df4dcc6ba24d71c9badb75d907e9
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-04-09 12:39:46 +0530

    initial commit

---
+++ src/main/java/lk/steam/rms/controller/RegistrationController.java
@@ -0,0 +1,9 @@
+package lk.steam.rms.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Registration")
+public class RegistrationController {
+}
