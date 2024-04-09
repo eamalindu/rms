49dd6bf3a84642e7b30f74880aef350f0957d008
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-04-09 16:40:57 +0530

    anotation added

--- src/main/java/lk/steam/rms/entity/RegistrationStatus.java
+++ src/main/java/lk/steam/rms/entity/RegistrationStatus.java
@@ -2,9 +2,16 @@ package lk.steam.rms.entity;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "registrationstatus")
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class RegistrationStatus {
 
     @Id
