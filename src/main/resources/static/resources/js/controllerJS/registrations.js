3056014a972a28f46046254f6a708ccf0028b488
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-04-09 15:09:26 +0530

    new functions introduce

--- src/main/resources/static/resources/js/controllerJS/registrations.js
+++ src/main/resources/static/resources/js/controllerJS/registrations.js
@@ -1,3 +1,16 @@
 window.addEventListener("load", () => {
 
+    //reset the registration form
+
+    //refresh the registration table
 });
+
+//creating a function to refresh the registrations table when ever needed
+const refreshRegistrationTable = ()=>{
+
+}
+
+//creating a function to reset the registrations form when ever needed
+const resetRegistrationForm=()=>{
+
+}
