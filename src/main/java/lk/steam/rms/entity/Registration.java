276d4b34feeb74a9c4eddbfd8b5ea2c7c72da023
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-04-09 12:30:37 +0530

    anotation added

--- src/main/java/lk/steam/rms/entity/Registration.java
+++ src/main/java/lk/steam/rms/entity/Registration.java
@@ -1,4 +1,16 @@
 package lk.steam.rms.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "registration")
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Registration {
 }
