f7bbad47aaef0d7d233031f7fe811a398d4295a7
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-04-09 15:45:09 +0530

    edited

--- src/main/java/lk/steam/rms/dao/RegistrationDAO.java
+++ src/main/java/lk/steam/rms/dao/RegistrationDAO.java
@@ -3,5 +3,5 @@ package lk.steam.rms.dao;
 import lk.steam.rms.entity.Registrations;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RegistrationDAO extends JpaRepository<Registration, Integer> {
+public interface RegistrationDAO extends JpaRepository<Registrations, Integer> {
 }
