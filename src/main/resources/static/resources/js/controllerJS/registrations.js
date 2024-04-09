14e4ba3fae39fef9c9b648420783ffd8afd6a2c5
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-04-09 15:09:45 +0530

    new functions used

--- src/main/resources/static/resources/js/controllerJS/registrations.js
+++ src/main/resources/static/resources/js/controllerJS/registrations.js
@@ -1,8 +1,9 @@
 window.addEventListener("load", () => {
 
     //reset the registration form
-
+    resetRegistrationForm();
     //refresh the registration table
+    refreshRegistrationTable();
 });
 
 //creating a function to refresh the registrations table when ever needed
