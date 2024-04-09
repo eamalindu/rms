8a87b5c8c5f4df77717e84da00ad815789791f7c
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-04-09 15:19:34 +0530

    edited

--- src/main/resources/static/resources/js/controllerJS/registrations.js
+++ src/main/resources/static/resources/js/controllerJS/registrations.js
@@ -14,4 +14,13 @@ const refreshRegistrationTable = ()=>{
 //creating a function to reset the registrations form when ever needed
 const resetRegistrationForm=()=>{
 
+    //initialize the 3rd party libraries (chosen)
+    $('.chosen-registration-search').chosen({width: '190px'});
+    $('#registrationSearchDateRange').daterangepicker({
+        "locale": {
+            "format": "YYYY-MM-DD",
+            //"separator": " to "
+        }
+    });
+
 }
