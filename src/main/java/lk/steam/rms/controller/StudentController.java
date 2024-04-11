2beccd6fd670db1af38d95141fec223f2c218785
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-04-11 22:41:02 +0530

    saveNewStudent function added

--- src/main/java/lk/steam/rms/controller/StudentController.java
+++ src/main/java/lk/steam/rms/controller/StudentController.java
@@ -1,9 +1,18 @@
 package lk.steam.rms.controller;
 
+import lk.steam.rms.entity.Student;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/Student")
 public class StudentController {
+
+    @PostMapping
+    public String saveNewStudent(@RequestBody Student student){
+        return "OK";
+    }
+
 }
