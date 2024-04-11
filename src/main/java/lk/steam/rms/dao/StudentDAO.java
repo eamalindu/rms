8bb68420ed83e44263c516a698b2f5b490755fa4
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-04-12 05:03:29 +0530

    custom Query added

--- src/main/java/lk/steam/rms/dao/StudentDAO.java
+++ src/main/java/lk/steam/rms/dao/StudentDAO.java
@@ -2,6 +2,11 @@ package lk.steam.rms.dao;
 
 import lk.steam.rms.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface StudentDAO extends JpaRepository<Student,Integer> {
+
+    @Query(value = "SELECT s from Student s where Student.idValue=?1")
+    Student getStudentsByIdValue(String idValue);
+
 }
