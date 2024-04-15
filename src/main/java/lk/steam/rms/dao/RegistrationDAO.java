287b83ea3796aa188018f8aefc9f679c2e13a029
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-04-15 13:24:07 +0530

    edited function name

--- src/main/java/lk/steam/rms/dao/RegistrationDAO.java
+++ src/main/java/lk/steam/rms/dao/RegistrationDAO.java
@@ -6,8 +6,8 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface RegistrationDAO extends JpaRepository<Registrations, Integer> {
 
-    //Get the next inquiry number form the database
-    //This data will be used in InquiryController
+    //Get the next registration number form the database
+    //This data will be used in RegistrationController
     @Query(value = "SELECT LPAD(MAX(reg.registrationnumber) + 1, 5, 0) AS registrationnumber FROM registration AS reg;",nativeQuery = true)
-    String getNextInquiryNumber();
+    String getNextRegistrationNumber();
 }
