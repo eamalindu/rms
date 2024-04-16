46b67f54becfbba6b9265d30f9cf1f23c6afd99b
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-04-16 19:43:28 +0530

    major update

---
+++ src/main/java/lk/steam/rms/controller/InstallmentPlanController.java
@@ -0,0 +1,91 @@
+package lk.steam.rms.controller;
+
+import lk.steam.rms.dao.InstallmentPlanDAO;
+import lk.steam.rms.dao.RegistrationDAO;
+import lk.steam.rms.dao.RegistrationStatusDAO;
+import lk.steam.rms.dao.StudentDAO;
+import lk.steam.rms.entity.InstallmentPlan;
+import lk.steam.rms.entity.RegistrationStatus;
+import lk.steam.rms.entity.Registrations;
+import lk.steam.rms.entity.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@RequestMapping("/InstallmentPlan")
+public class InstallmentPlanController {
+    @Autowired
+    private RegistrationDAO registrationDAO;
+
+    @Autowired
+    private StudentDAO studentDAO;
+
+    @Autowired
+    private RegistrationStatusDAO registrationStatusDAO;
+
+    @Autowired
+    private InstallmentPlanDAO installmentPlanDAO;
+
+    @PostMapping
+    public String saveNewRegistrationWithInstallments(@RequestBody List<InstallmentPlan> installmentPlanList){
+
+        try{
+
+            Registrations currentRegistration = installmentPlanList.get(0).getRegistrationID();
+
+            //set Registration Number
+            String regNextNumber = registrationDAO.getNextRegistrationNumber();
+            if (regNextNumber == null || regNextNumber.length() == 0 || regNextNumber.isEmpty()) {
+                currentRegistration.setRegistrationNumber("00001");
+            } else {
+                currentRegistration.setRegistrationNumber(regNextNumber);
+            }
+
+            currentRegistration.setTimestamp(LocalDateTime.now());
+            currentRegistration.setAddedBy("User1");
+            currentRegistration.setCommissionPaidTo("User1");
+            //Student sample = studentDAO.getReferenceById(1);
+            RegistrationStatus sampleStatus = registrationStatusDAO.getReferenceById(1);
+            //registrations.setStudentID(sample);
+            currentRegistration.setRegistrationStatusID(sampleStatus);
+
+            //store the current student nic
+            String currentIdValue = currentRegistration.getStudentID().getIdValue();
+
+            //check this student exsist
+            Student exsistStudent = studentDAO.getStudentsByIdValue(currentIdValue);
+
+            if (exsistStudent == null) {
+                Student registeredStudent = currentRegistration.getStudentID();
+                //set auto increment values and backend generated values
+                registeredStudent.setStudentNumber("ST-0003");
+                //save the student
+                studentDAO.save(registeredStudent);
+                //set that student as the registration
+            } else {
+
+                currentRegistration.setStudentID(exsistStudent);
+
+            }
+            Registrations completedRegistration = registrationDAO.save(currentRegistration);
+            for (InstallmentPlan installmentPlan : installmentPlanList) {
+                installmentPlan.setRegistrationID(completedRegistration);
+                installmentPlanDAO.save(installmentPlan);
+            }
+
+
+
+            return "OK";
+        }
+        catch (Exception ex){
+            return "Save Failed "+ex.getMessage();
+        }
+
+    }
+}
