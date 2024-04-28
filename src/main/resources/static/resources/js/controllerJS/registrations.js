ec96a974adddf6a45e749e30e57babd864dd94b7
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-04-28 11:55:45 +0530

    edited

--- src/main/resources/static/resources/js/controllerJS/registrations.js
+++ src/main/resources/static/resources/js/controllerJS/registrations.js
@@ -170,6 +170,7 @@ const rowView=(ob,index)=>{
         StudentModalMobile.value =  studentRecordToBeEdited.mobileNumber;
         StudentModalAddress1.value =  studentRecordToBeEdited.addressLine1;
         StudentModalAddress2.value =  studentRecordToBeEdited.addressLine2;
+        StudentModalCity.value =  studentRecordToBeEdited.city;
 
 
     })
