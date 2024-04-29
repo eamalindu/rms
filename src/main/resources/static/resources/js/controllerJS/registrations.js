02b8cd1e319fa38e36a3669d719eacaede53ff00
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-04-29 10:11:55 +0530

    IdValue set

--- src/main/resources/static/resources/js/controllerJS/registrations.js
+++ src/main/resources/static/resources/js/controllerJS/registrations.js
@@ -172,6 +172,7 @@ const rowView=(ob,index)=>{
         StudentModalAddress2.value =  studentRecordToBeEdited.addressLine2;
         StudentModalCity.value =  studentRecordToBeEdited.city;
         StudentModalEmail.value = studentRecordToBeEdited.email;
+        StudentModalIDValue.value = studentRecordToBeEdited.idValue;
 
 
     })
