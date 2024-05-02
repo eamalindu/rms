97fb1b4bf943501096f5c00ae4f20aaa2476f047
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-05-02 11:11:13 +0530

    new custom query added

--- src/main/java/lk/steam/rms/dao/InstallmentPlanDAO.java
+++ src/main/java/lk/steam/rms/dao/InstallmentPlanDAO.java
@@ -2,6 +2,13 @@ package lk.steam.rms.dao;
 
 import lk.steam.rms.entity.InstallmentPlan;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface InstallmentPlanDAO extends JpaRepository<InstallmentPlan,Integer> {
+
+    @Query(value = "select i from InstallmentPlan i where i.registrationID.registrationNumber=?1")
+    List<InstallmentPlan> getInstallmentPlanByRegistrationNumber(String registrationNumber);
+
 }
