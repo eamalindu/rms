9ba01655057ad2384b400381ea302093daf24d86
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-05-05 21:52:17 +0530

    post mapping added

--- src/main/java/lk/steam/rms/controller/PaymentController.java
+++ src/main/java/lk/steam/rms/controller/PaymentController.java
@@ -1,9 +1,32 @@
 package lk.steam.rms.controller;
 
+import lk.steam.rms.dao.InstallmentPlanDAO;
+import lk.steam.rms.dao.PaymentDAO;
+import lk.steam.rms.dao.RegistrationDAO;
+import lk.steam.rms.entity.Payment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/Payment")
 public class PaymentController {
+
+    @Autowired
+    private PaymentDAO paymentDAO;
+
+    @Autowired
+    private RegistrationDAO registrationDAO;
+
+    @Autowired
+    private InstallmentPlanDAO installmentPlanDAO;
+
+    @PostMapping
+    public String saveNewPayment(@RequestBody Payment payment){
+
+
+
+    }
 }
