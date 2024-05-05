9cea5993bd1ab15aa90b9ee12ab3541cafb8cd23
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-05-05 11:13:58 +0530

    id property

--- src/main/java/lk/steam/rms/entity/Payment.java
+++ src/main/java/lk/steam/rms/entity/Payment.java
@@ -1,4 +1,19 @@
 package lk.steam.rms.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "payment")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Payment {
+
+    @Id
+    @Column(name = "id",unique = true)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 }
