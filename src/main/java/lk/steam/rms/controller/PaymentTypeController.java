b12e49d2ac26733fdd36e5ca045fca93aa7e2f66
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-05-05 11:59:21 +0530

    findAll mapped

--- src/main/java/lk/steam/rms/controller/PaymentTypeController.java
+++ src/main/java/lk/steam/rms/controller/PaymentTypeController.java
@@ -1,4 +1,23 @@
 package lk.steam.rms.controller;
 
+
+import lk.steam.rms.dao.PaymentTypeDAO;
+import lk.steam.rms.entity.PaymentType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/PaymentType")
 public class PaymentTypeController {
+    @Autowired
+    private PaymentTypeDAO paymentTypeDAO;
+
+    @GetMapping(value = "/findall",produces = "application/json")
+    public List<PaymentType> findAll(){
+        return paymentTypeDAO.findAll();
+    }
 }
