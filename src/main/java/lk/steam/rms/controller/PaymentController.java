7caf2138709f7d9e31bf1768a7d1813ad5864e24
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-05-05 21:42:46 +0530

    edited

--- src/main/java/lk/steam/rms/controller/PaymentController.java
+++ src/main/java/lk/steam/rms/controller/PaymentController.java
@@ -1,4 +1,9 @@
 package lk.steam.rms.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/Payment")
 public class PaymentController {
 }
