30a98da84c6cf7a191205930f9f447652c492ff7
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-05-05 22:40:54 +0530

    edited

--- src/main/java/lk/steam/rms/entity/Payment.java
+++ src/main/java/lk/steam/rms/entity/Payment.java
@@ -33,7 +33,7 @@ public class Payment {
     @NotNull
     private String addedBy;
 
-    @Column(name = "installmentId")
+    @Column(name = "installmentid")
     private Integer installmentID;
 
     @ManyToOne
