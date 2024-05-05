4e40f81745a06c02a009573d5c667c1182f89961
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-05-05 22:56:42 +0530

    edited

--- src/main/java/lk/steam/rms/controller/PaymentController.java
+++ src/main/java/lk/steam/rms/controller/PaymentController.java
@@ -3,6 +3,7 @@ package lk.steam.rms.controller;
 import lk.steam.rms.dao.InstallmentPlanDAO;
 import lk.steam.rms.dao.PaymentDAO;
 import lk.steam.rms.dao.RegistrationDAO;
+import lk.steam.rms.dao.RegistrationStatusDAO;
 import lk.steam.rms.entity.Payment;
 import lk.steam.rms.entity.Registrations;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,6 +27,9 @@ public class PaymentController {
 
     @Autowired
     private InstallmentPlanDAO installmentPlanDAO;
+
+    @Autowired
+    private RegistrationStatusDAO registrationStatusDAO;
 
     @PostMapping
     public String saveNewPayment(@RequestBody Payment payment){
@@ -54,6 +58,18 @@ public class PaymentController {
         currentRegistration.setBalanceAmount(updatedBalance);
 
         //change the registration status
+        if(currentRegistration.getRegistrationStatusID().getName().equals("Pending")){
+            BigDecimal currentRegistrationRegistrationFee = currentRegistration.getBatchID().getPaymentPlanID().getRegistrationFee();
+            BigDecimal currentPaymentAmount = payment.getAmount();
+
+            if (currentPaymentAmount.compareTo(currentRegistrationRegistrationFee) >= 0) {
+                // currentPaymentAmount is greater than or equal to currentRegistrationRegistrationFee
+                // Handle the case where the payment amount is sufficient or more
+                currentRegistration.setRegistrationStatusID(registrationStatusDAO.getReferenceById(1));
+
+            }
+
+        }
 
         registrationDAO.save(currentRegistration);
 
