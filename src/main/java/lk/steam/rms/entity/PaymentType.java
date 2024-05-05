0f8b5de425303f14092d66f4937c5b8228f5ae25
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-05-05 11:37:14 +0530

    edited

--- src/main/java/lk/steam/rms/entity/PaymentType.java
+++ src/main/java/lk/steam/rms/entity/PaymentType.java
@@ -1,7 +1,7 @@
 package lk.steam.rms.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,4 +12,12 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class PaymentType {
+    @Id
+    @Column(name = "id",unique = true)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name")
+    @NotNull
+    private String name;
 }
