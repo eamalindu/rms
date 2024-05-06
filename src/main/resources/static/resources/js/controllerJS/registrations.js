78e908a5eb0b23100a8f47eb41e35939e2c4406e
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-05-06 22:46:31 +0530

    major update

--- src/main/resources/static/resources/js/controllerJS/registrations.js
+++ src/main/resources/static/resources/js/controllerJS/registrations.js
@@ -265,10 +265,9 @@ const rowView=(ob,index)=>{
         {property:getPaymentMethod,dataType:'function'},
         {property:getPaymentAmount,dataType:'function'},
         {property:'addedBy',dataType:'text'},
-        {property:getInvoice,dataType:'function'},
     ];
 
-    fillDataIntoTableWithOutAction(tblPaymentHistory,paymentHistory,displayPropertyListForPaymentHistory)
+    fillDataIntoTableWithPrint(tblPaymentHistory,paymentHistory,displayPropertyListForPaymentHistory,printInvoice)
 
     //This code snippet will save the current object student sub object to the global variable studentRecordToBeEdited;
     registrationSheetStudentID.addEventListener("click",()=>{
@@ -377,14 +376,19 @@ const getPaymentAmount = (ob)=>{
     return "Rs. "+ob.amount.toLocaleString('en-US',{maximumFractionDigits:2,minimumFractionDigits:2});
 }
 
-const getInvoice =(ob)=>{
+// const getInvoice =(ob)=>{
+//
+//     return ' <button class="btn btn-sm btn-secondary rounded-0 small" style="width: 30px;height: 30px;"\n' +
+//         '                                                onclick="window.location.href=\'invoice.html\'">\n' +
+//         '                                            <i class="fa-solid fa-print small"></i>\n' +
+//         '                                        </button>'
+// }
 
-    return ' <button class="btn btn-sm btn-secondary rounded-0 small" style="width: 30px;height: 30px;"\n' +
-        '                                                onclick="window.location.href=\'invoice.html\'">\n' +
-        '                                            <i class="fa-solid fa-print small"></i>\n' +
-        '                                        </button>'
+
+const printInvoice = (object,index)=>{
+    alert("hehe");
+
 }
-
 //reset chosen select using jquery
 $('#btn-reset').on('click', function () {
     setTimeout(function () {
