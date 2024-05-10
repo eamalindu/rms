565aa8a9c0197644c65b8ac6b65e3a6e16025be6
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-05-10 23:58:40 +0530

    registrationEdit edited

--- src/main/resources/static/resources/js/controllerJS/registrations.js
+++ src/main/resources/static/resources/js/controllerJS/registrations.js
@@ -572,4 +572,16 @@ const registrationEdit=()=>{
 
     //display the update button once the edit button is clicked
     btnRegistrationSheetUpdate.style.display = 'block';
+
+    //remove the attribute readonly to make inputs accept the user input values
+    //give a border color to inputs indicate that the input's values are ready to be edited
+    inputs = document.querySelectorAll('.registrationSheetInputs');
+
+    //remove the disabled attribute from the select
+    //give a border color to indicate that select can be now edited
+
+    inputs.forEach(function (input) {
+        input.removeAttribute('disabled');
+        input.setAttribute('style', 'border:1px solid #0DCAF0!important;background-color:rgba(13,202,240,0.2);');
+    });
 }
