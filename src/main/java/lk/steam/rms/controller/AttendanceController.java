335d1b1fefff2d87657053b746d55ac3d14b3821
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-05-15 21:42:45 +0530

    edited

--- src/main/java/lk/steam/rms/controller/AttendanceController.java
+++ src/main/java/lk/steam/rms/controller/AttendanceController.java
@@ -1,4 +1,10 @@
 package lk.steam.rms.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/Attendance")
 public class AttendanceController {
+
 }
