78b2953ea63524249c59354cf9737f1d4bd8cd0e
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-05-15 21:43:35 +0530

    UI mapped

--- src/main/java/lk/steam/rms/controller/AttendanceController.java
+++ src/main/java/lk/steam/rms/controller/AttendanceController.java
@@ -1,10 +1,18 @@
 package lk.steam.rms.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 @RequestMapping(value = "/Attendance")
 public class AttendanceController {
 
+    @GetMapping()
+    public ModelAndView attendanceUI() {
+        ModelAndView attendanceView = new ModelAndView();
+        attendanceView.setViewName("attendance.html");
+        return attendanceView;
+    }
 }
