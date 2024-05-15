be06363b33c73ec143ce3e7c4d9cf7bd3fbdb776
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-05-15 20:23:55 +0530

    edited

--- src/main/resources/static/resources/js/controllerJS/batch.js
+++ src/main/resources/static/resources/js/controllerJS/batch.js
@@ -332,7 +332,7 @@ const getScheduleEndTime = (ob)=>{
 }
 
 const getClassRoom = (ob)=>{
-    return ob.lectureRoomID.code+", "+ob.lectureRoomID.floor;
+    return ob.lectureRoomID.name+", "+ob.lectureRoomID.floor;
 }
 
 //creating a function to reset the Batch form when ever needed
