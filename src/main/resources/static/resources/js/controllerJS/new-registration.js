1d2cfe1a2af26fd0a4902e1978083b3383cf21e7
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-05-17 22:34:57 +0530

    edited

--- src/main/resources/static/resources/js/controllerJS/new-registration.js
+++ src/main/resources/static/resources/js/controllerJS/new-registration.js
@@ -690,3 +690,57 @@ const resetPaymentForm = ()=>{
 
 
 }
+const newPaymentSubmit=()=>{
+
+    //attach the current registration object to the payment object
+    //get the current registration object from rowView function
+    newPayment.registrationID =registration;
+    let errors = checkPaymentFormErrors();
+    if(errors==='') {
+        showCustomConfirm("You are about to add a New Payment of <br><span class='text-steam-green'>Rs. " + parseFloat(newPayment.amount).toLocaleString('en-US', {
+            maximumFractionDigits: 2,
+            minimumFractionDigits: 2
+        }) + "</span> to the registration : <span class='text-steam-green'>" + registration.registrationNumber + "</span><br><br>Are You Sure?", function (result) {
+            if (result) {
+                let serviceResponse = ajaxHttpRequest("/Payment", 'POST', newPayment);
+                if (serviceResponse === "OK") {
+                    //this means data successfully passed to the backend
+                    //show an alert to user
+                    showCustomModal("Payment Successfully Added!", "success");
+
+
+                } else {
+                    //this means there was a problem with the query
+                    //shows an error alert to the user
+                    showCustomModal("Operation Failed! <br>" + serviceResponse.responseJSON.error + " <span class='small'>(" + serviceResponse.responseJSON.status + ")</span>", "error");
+                }
+            }
+        });
+    }
+    else{
+
+        //there are errors
+        //display them to the user using external-ModalFunction()
+        showCustomModal(errors, 'warning');
+    }
+}
+
+const checkPaymentFormErrors = ()=>{
+    //check for binding
+    //0 isn't allowed as a payment
+    // cant be larger than Total Outstanding
+    let errors = ''
+    if(newPayment.paymentTypeID==null){
+        errors = errors + 'Payment Type is Required<br>';
+    }
+    if(newPayment.amount==null){
+        errors = errors + 'Amount is Required<br>';
+    }
+    if(newPayment.amount<=0){
+        errors = errors + 'Amount Can Not Be Rs. 0.00<br>';
+    }
+    if(newPayment.amount>registration.balanceAmount){
+        errors = errors +'The Current amount <span class="text-steam-green">Rs. '+newPayment.amount+ '.00</span> exceeds the total outstanding balance <span class="text-steam-green">Rs. '+registration.balanceAmount+'.00</span><br>';
+    }
+    return errors;
+}
