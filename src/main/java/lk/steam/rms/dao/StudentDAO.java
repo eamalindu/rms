84ff71a9ab07e636967e354db11ff914df060f86
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-05-18 14:07:49 +0530

    custom query added

--- src/main/java/lk/steam/rms/dao/StudentDAO.java
+++ src/main/java/lk/steam/rms/dao/StudentDAO.java
@@ -9,6 +9,9 @@ public interface StudentDAO extends JpaRepository<Student,Integer> {
     @Query(value = "SELECT s from Student s where s.idValue=?1")
     Student getStudentsByIdValue(String idValue);
 
+    @Query(value = "SELECT s from Student s where (s.idValue=?1) or (s.studentNumber=?1) or(s.mobileNumber=?1)")
+    Student getStudentsByNicOrStudentNumberOrMobileNumber(String searchText);
+
     @Query(value = "select concat('ST-',(lpad(substring(max(studentnumber),5)+1,4 ,0))) from student",nativeQuery = true)
     String getNextStudentNumber();
 
