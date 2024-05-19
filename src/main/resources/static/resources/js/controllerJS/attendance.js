a015baa45bf6c10fad5db4cda691316b4d67f205
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-05-20 01:19:33 +0530

    edited

--- src/main/resources/static/resources/js/controllerJS/attendance.js
+++ src/main/resources/static/resources/js/controllerJS/attendance.js
@@ -34,8 +34,8 @@ const createBatchesConductingToday = (dataList, containerID) => {
 
 
             div.innerHTML = `<div class="w-75 ">
-                                    <span class="text-muted small ">Course | Batch | Location</span>
-                                    <p class="mb-0">${element.courseID.code} / <span>${element.batchCode}</span> / <span>${correctSchedule.lectureRoomID.code}  - ${correctSchedule.lectureRoomID.floor}</span><small> [${correctSchedule.startTime.slice(0, -3)} - ${correctSchedule.endTime.slice(0, -3)}]</small></p>
+                                    <span class="text-muted small ">Course | Batch | Location | Schdule</span>
+                                    <p class="mb-0">${element.courseID.code} / <span>${element.batchCode}</span> / <span>${correctSchedule.lectureRoomID.code}  - ${correctSchedule.lectureRoomID.floor}</span> / ${correctSchedule.startTime.slice(0, -3)} - ${correctSchedule.endTime.slice(0, -3)}</p>
                                 </div>
                                 <div class="w-25 text-end">
                                     <span class="text-muted small">Attendance</span>
