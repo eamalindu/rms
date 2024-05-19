641245e919393c9354da27cebbf85ff3afaf89c5
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-05-20 00:12:27 +0530

    comment added

--- src/main/resources/static/resources/js/controllerJS/attendance.js
+++ src/main/resources/static/resources/js/controllerJS/attendance.js
@@ -2,6 +2,11 @@ window.addEventListener("load", function () {
 
 });
 
+//This function will generate batch information that will conduct today
+//This function has two arguments
+//1) dataList -> data Array which contain actual data from the database
+//2) containerID -> element id of where the divs should display
+//This function is called using window.load event handler
 const createBatchesConductingToday = (dataList, containerID) => {
     containerID.innerHTML = '';
     if (dataList.length < 0) {
