2a0485d4e1dfae037cb1f06fa213d12e79035da9
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-05-20 00:01:12 +0530

    query edited

--- src/main/java/lk/steam/rms/dao/BatchDAO.java
+++ src/main/java/lk/steam/rms/dao/BatchDAO.java
@@ -24,7 +24,7 @@ public interface BatchDAO extends JpaRepository<Batch,Integer> {
     @Query("SELECT b FROM Batch b WHERE b.batchCode=?1")
     List<Batch> getBatchInfoByBatchCode(String BatchCode);
 
-    @Query("SELECT b from Batch b JOIN BatchHasDay bhd on b.id = bhd.batchID.id JOIN Day d ON bhd.dayID.id = d.id where d.name = DAYNAME(CURDATE())")
+    @Query("SELECT b from Batch b JOIN BatchHasDay bhd on b.id = bhd.batchID.id JOIN Day d ON bhd.dayID.id = d.id where d.name = DAYNAME(CURDATE()) and b.batchStatusID.id=2")
     List<Batch> getBatchesConductToday();
 
 
