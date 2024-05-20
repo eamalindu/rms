210b80697c1360e7324be74fcf9b8089142e6df3
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-05-20 21:25:18 +0530

    custom query added

--- src/main/java/lk/steam/rms/dao/AttendanceDAO.java
+++ src/main/java/lk/steam/rms/dao/AttendanceDAO.java
@@ -4,10 +4,14 @@ import lk.steam.rms.entity.Attendance;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface AttendanceDAO extends JpaRepository<Attendance,Integer> {
 
     @Query(value = "SELECT a from Attendance a where a.batchID=?1")
     List<Attendance> getAttendanceByBatchID(Integer batchID);
+
+    @Query(value = "SELECT * from attendance where batchid=?1 and registration_id=?2 and date(timestamp)=curdate()",nativeQuery = true)
+    Attendance getAttendanceByBatchIDAndRegistrationID(Integer batchID, Integer registrationID);
 }
