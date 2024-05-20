4f5791189c4bb24ce46d6e5214ef3ea40674d023
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-05-20 13:38:37 +0530

    property mapped

--- src/main/java/lk/steam/rms/entity/Attendance.java
+++ src/main/java/lk/steam/rms/entity/Attendance.java
@@ -1,9 +1,12 @@
 package lk.steam.rms.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "attendance")
@@ -16,5 +19,15 @@ public class Attendance {
     @Column(name = "id",unique = true)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @Column(name = "batchid")
+    @NotNull
+    private Integer batchID;
+
+    @Column(name = "timestamp")
+    @NotNull
+    private LocalDateTime timeStamp;
+
+
 
 }
