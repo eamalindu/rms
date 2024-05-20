51d938f8f31b12bd0a5e1793c6840a2f826aa740
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-05-20 13:31:18 +0530

    general config

--- src/main/java/lk/steam/rms/entity/Attendance.java
+++ src/main/java/lk/steam/rms/entity/Attendance.java
@@ -1,4 +1,15 @@
 package lk.steam.rms.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "attendance")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Attendance {
 }
