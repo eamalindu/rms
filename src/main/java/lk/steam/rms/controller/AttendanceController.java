bb271ea80e9af57916dda402b2664645f7ea13f0
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-05-20 13:54:15 +0530

    new service added

--- src/main/java/lk/steam/rms/controller/AttendanceController.java
+++ src/main/java/lk/steam/rms/controller/AttendanceController.java
@@ -4,6 +4,7 @@ import lk.steam.rms.dao.AttendanceDAO;
 import lk.steam.rms.entity.Attendance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -30,4 +31,7 @@ public class AttendanceController {
     }
 
     @GetMapping(value = "/getAttendanceByBatchID/{batchID}",produces = "application/json")
+    List<Attendance> getAttendanceByBatchID(@PathVariable Integer batchID){
+        return attendanceDAO.getAttendanceByBatchID(batchID);
+    }
 }
