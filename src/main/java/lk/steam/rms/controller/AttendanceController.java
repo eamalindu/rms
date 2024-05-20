41da281a56032952d0ad81989b9278b598a2f7b6
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-05-20 13:44:29 +0530

    edited

--- src/main/java/lk/steam/rms/controller/AttendanceController.java
+++ src/main/java/lk/steam/rms/controller/AttendanceController.java
@@ -1,18 +1,31 @@
 package lk.steam.rms.controller;
 
+import lk.steam.rms.dao.AttendanceDAO;
+import lk.steam.rms.entity.Attendance;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "/Attendance")
 public class AttendanceController {
+
+    @Autowired
+    private AttendanceDAO attendanceDAO;
 
     @GetMapping()
     public ModelAndView attendanceUI() {
         ModelAndView attendanceView = new ModelAndView();
         attendanceView.setViewName("attendance.html");
         return attendanceView;
+    }
+
+    @GetMapping("/findall")
+    List<Attendance> findAll(){
+        return attendanceDAO.findAll();
     }
 }
