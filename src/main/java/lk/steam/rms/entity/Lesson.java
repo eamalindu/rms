b91193a7528cf8d3275c9e991a310b4f978e583f
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-05-21 18:57:47 +0530

    initial commit

---
+++ src/main/java/lk/steam/rms/entity/Lesson.java
@@ -0,0 +1,32 @@
+package lk.steam.rms.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "lesson")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Lesson {
+
+    @Id
+    @Column(name = "id",unique = true)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name")
+    @NotNull
+    private String name;
+
+    @Column(name = "code")
+    @NotNull
+    private String code;
+
+    @Column(name = "examavailable")
+    @NotNull
+    private Boolean examAvailable;
+}
