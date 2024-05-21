88a0a5c4d047b2f3d3753c63f1a3ffa4400216c2
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-05-21 19:11:29 +0530

    findAll service added

--- src/main/java/lk/steam/rms/controller/LessonController.java
+++ src/main/java/lk/steam/rms/controller/LessonController.java
@@ -1,9 +1,23 @@
 package lk.steam.rms.controller;
 
+import lk.steam.rms.dao.LessonDAO;
+import lk.steam.rms.entity.Lesson;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/Lesson")
 public class LessonController {
+
+    @Autowired
+    private LessonDAO lessonDAO;
+
+    @GetMapping(value = "/findall",produces = "application/json")
+    List<Lesson> findAll(){
+        return lessonDAO.findAll();
+    }
 }
