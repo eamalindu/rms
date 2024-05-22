e4acb0648e73c981b9752dca886cc0a210e1dd3e
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-05-22 19:39:54 +0530

    edited

--- src/main/java/lk/steam/rms/controller/LessonController.java
+++ src/main/java/lk/steam/rms/controller/LessonController.java
@@ -3,9 +3,7 @@ package lk.steam.rms.controller;
 import lk.steam.rms.dao.LessonDAO;
 import lk.steam.rms.entity.Lesson;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,7 +15,13 @@ public class LessonController {
     private LessonDAO lessonDAO;
 
     @GetMapping(value = "/findall",produces = "application/json")
-    List<Lesson> findAll(){
+    public List<Lesson> findAll(){
         return lessonDAO.findAll();
+    }
+
+    @PostMapping
+    public String saveNewLesson(@RequestBody Lesson lesson){
+        lessonDAO.save(lesson);
+        return "OK";
     }
 }
