6d7f63149de6e6c17500548affeaf656203b4225
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-05-23 13:19:44 +0530

    query added

--- src/main/java/lk/steam/rms/dao/LessonDAO.java
+++ src/main/java/lk/steam/rms/dao/LessonDAO.java
@@ -2,6 +2,10 @@ package lk.steam.rms.dao;
 
 import lk.steam.rms.entity.Lesson;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface LessonDAO extends JpaRepository<Lesson,Integer> {
+
+    @Query(value = "SELECT l from Lesson l where l.code=?1")
+    Lesson getLessonByCode(String lessonCode);
 }
