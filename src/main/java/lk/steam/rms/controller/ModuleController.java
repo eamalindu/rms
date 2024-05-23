d90d92418b5575e68695cafc156dda19d577d0d7
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-05-23 14:25:38 +0530

    major update

--- src/main/java/lk/steam/rms/controller/ModuleController.java
+++ src/main/java/lk/steam/rms/controller/ModuleController.java
@@ -1,6 +1,7 @@
 package lk.steam.rms.controller;
 
 import lk.steam.rms.dao.ModuleDAO;
+import lk.steam.rms.entity.Module;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
