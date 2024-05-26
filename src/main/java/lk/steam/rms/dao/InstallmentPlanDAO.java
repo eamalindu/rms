c3f470f312c9f0c977afac666280e2d0b1b3a56b
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-05-26 14:26:22 +0530

    custom query added

--- src/main/java/lk/steam/rms/dao/InstallmentPlanDAO.java
+++ src/main/java/lk/steam/rms/dao/InstallmentPlanDAO.java
@@ -4,11 +4,16 @@ import lk.steam.rms.entity.InstallmentPlan;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public interface InstallmentPlanDAO extends JpaRepository<InstallmentPlan,Integer> {
 
     @Query(value = "select i from InstallmentPlan i where i.registrationID.id=?1")
     List<InstallmentPlan> getInstallmentPlanByRegistrationID(Integer registrationID);
+
+    //get due payment from registration id
+    @Query(value = "SELECT sum(i.balanceAmount) from InstallmentPlan i where i.dueDate<=date(now()) and i.registrationID.id=?1")
+    BigDecimal getDueInstallmentAmountFromRegistrationID(Integer registrationID);
 
 }
