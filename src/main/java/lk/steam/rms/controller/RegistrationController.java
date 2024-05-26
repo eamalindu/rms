afff39bc21f308df1c7485b3222b167160fe3190
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-05-26 20:40:01 +0530

    edited

--- src/main/java/lk/steam/rms/controller/RegistrationController.java
+++ src/main/java/lk/steam/rms/controller/RegistrationController.java
@@ -1,13 +1,11 @@
 package lk.steam.rms.controller;
 
 import jakarta.transaction.Transactional;
+import lk.steam.rms.dao.InquiryDAO;
 import lk.steam.rms.dao.RegistrationDAO;
 import lk.steam.rms.dao.RegistrationStatusDAO;
 import lk.steam.rms.dao.StudentDAO;
-import lk.steam.rms.entity.Batch;
-import lk.steam.rms.entity.RegistrationStatus;
-import lk.steam.rms.entity.Registrations;
-import lk.steam.rms.entity.Student;
+import lk.steam.rms.entity.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
@@ -29,6 +27,9 @@ public class RegistrationController {
 
     @Autowired
     private RegistrationStatusDAO registrationStatusDAO;
+
+    @Autowired
+    private InquiryDAO inquiryDAO;
 
     @GetMapping()
     public ModelAndView registrationUI() {
@@ -88,6 +89,9 @@ public class RegistrationController {
                 registrations.setStudentID(exsistStudent);
 
             }
+
+            //check an inquiry is available for the current registration
+            Inquiry currentInquiry = inquiryDAO.getActiveInquiryByIDAndCourseId(registrations.getStudentID().getIdValue(),registrations.getCourseID().getId());
 
             Registrations completedRegistration = registrationDAO.save(registrations);
             System.out.println(completedRegistration.getId());
