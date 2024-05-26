097bde83a37a908d6c1254dee09687ce30710018
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-05-27 00:10:16 +0530

    edited

--- src/main/java/lk/steam/rms/controller/ResetPasswordController.java
+++ src/main/java/lk/steam/rms/controller/ResetPasswordController.java
@@ -1,7 +1,14 @@
 package lk.steam.rms.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class ResetPasswordController {
+
+    @GetMapping(value = "/Reset-Password")
+    public ModelAndView resetPassword() {
+
+    }
 }
