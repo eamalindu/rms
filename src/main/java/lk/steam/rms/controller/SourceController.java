bd1915e4b6a532161758be121251aaa1aab2a980
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-05-26 20:34:20 +0530

    edited

--- src/main/java/lk/steam/rms/controller/SourceController.java
+++ src/main/java/lk/steam/rms/controller/SourceController.java
@@ -1,7 +1,7 @@
 package lk.steam.rms.controller;
 
-import lk.steam.ims.dao.SourceDAO;
-import lk.steam.ims.entity.Source;
+import lk.steam.rms.dao.SourceDAO;
+import lk.steam.rms.entity.Source;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
