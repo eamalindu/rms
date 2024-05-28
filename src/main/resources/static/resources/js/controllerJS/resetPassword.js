d7659880e443d8e4935d0b3810de2673cf96eb73
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-05-28 20:31:59 +0530

    major update

--- src/main/resources/static/resources/js/controllerJS/resetPassword.js
+++ src/main/resources/static/resources/js/controllerJS/resetPassword.js
@@ -1,22 +1,51 @@
-const sendOTP = ()=>{
-    showCustomModal("Email Contain OTP sent successfully","success")
+window.addEventListener("load", () => {
+    password.disabled = true;
+    btnSendOtp.disabled = true;
+
+    username.addEventListener('keyup', () => {
+        btnSendOtp.disabled = false;
+        if (username.value == '') {
+            btnSendOtp.disabled = true;
+        }
+
+    })
+
+})
+
+
+const sendOTP = () => {
+    countdown(2, countdownText, 'You can request another OTP after');
+
+    btnSendOtp.disabled = true;
+    username.disabled = true;
+    btnLogin.disabled = false;
+
+    otpContainer.classList.remove('d-none');
+    //check an account is present
+    //send a email
+    //display a user msg
+    password.disabled = false;
+    password.focus();
+    showCustomModal("Email Contain OTP sent successfully", "success");
 }
 
-const countdown=(minutes,elementID,msg)=> {
+const countdown = (minutes, elementID, msg) => {
     var seconds = 60;
     var mins = minutes
+
     function tick() {
         var counter = elementID;
-        var current_minutes = mins-1
+        var current_minutes = mins - 1
         seconds--;
-        counter.innerHTML ="<small>"+msg+" 0"+ current_minutes.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds)+" mintues</small>";
-        if( seconds > 0 ) {
+        counter.innerHTML = "<small>" + msg + " 0" + current_minutes.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds) + " mintues</small>";
+        if (seconds > 0) {
             setTimeout(tick, 1000);
         } else {
-            if(mins > 1){
-                countdown(mins-1);
+            if (mins > 1) {
+                countdown(mins - 1);
             }
         }
     }
+
     tick();
 }
