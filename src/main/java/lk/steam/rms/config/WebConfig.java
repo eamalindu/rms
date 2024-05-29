2f78e28247e51728813e64165ec8c7b55d3bb238
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-05-29 22:56:49 +0530

    major update [auto logout if no activity is detected]

--- src/main/java/lk/steam/rms/config/WebConfig.java
+++ src/main/java/lk/steam/rms/config/WebConfig.java
@@ -15,58 +15,44 @@ public class WebConfig {
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
 
         httpSecurity.authorizeHttpRequests(auth -> {
-            auth
-                    .requestMatchers("/login").permitAll()
-                    .requestMatchers("/Reset-Password/**").permitAll()
-                    .requestMatchers("/CreateAdmin").permitAll()
-                    .requestMatchers("/Employee/**").hasAnyAuthority("Admin","Manager")
-                    .requestMatchers("/User/**").hasAnyAuthority("Admin","Manager")
-                    .requestMatchers("/Administrations/**").hasAnyAuthority("Admin","Manager")
-                    .requestMatchers("/error").permitAll()
-                    .requestMatchers("/resources/**").permitAll()
-                    .requestMatchers("/Course/**").hasAnyAuthority("Admin","Manager","Coordinator","Counsellor")
-                    .requestMatchers("/Batch/**").hasAnyAuthority("Admin","Manager","Coordinator","Counsellor")
-                    .requestMatchers("/Batch").hasAnyAuthority("Admin","Manager","Coordinator","Counsellor")
-                    .requestMatchers("/Attendance").hasAnyAuthority("Admin","Manager","Counsellor")
-                    .anyRequest().authenticated();
+                    auth.requestMatchers("/login").permitAll().requestMatchers("/Reset-Password/**").permitAll().requestMatchers("/CreateAdmin").permitAll().requestMatchers("/Employee/**").hasAnyAuthority("Admin", "Manager").requestMatchers("/User/**").hasAnyAuthority("Admin", "Manager").requestMatchers("/Administrations/**").hasAnyAuthority("Admin", "Manager").requestMatchers("/error").permitAll().requestMatchers("/resources/**").permitAll().requestMatchers("/Course/**").hasAnyAuthority("Admin", "Manager", "Coordinator", "Counsellor").requestMatchers("/Batch/**").hasAnyAuthority("Admin", "Manager", "Coordinator", "Counsellor").requestMatchers("/Batch").hasAnyAuthority("Admin", "Manager", "Coordinator", "Counsellor").requestMatchers("/Attendance").hasAnyAuthority("Admin", "Manager", "Counsellor").anyRequest().authenticated();
 
-        })
+                })
                 //login handling
-                .formLogin(login ->{
-                    login
-                            .loginPage("/login")
-                            .defaultSuccessUrl("/Dashboard",true)
-                            .failureUrl("/login?error=failed")
-                            .usernameParameter("username")
-                            .passwordParameter("password");
+                .formLogin(login -> {
+                    login.loginPage("/login").defaultSuccessUrl("/Dashboard", true).failureUrl("/login?error=failed").usernameParameter("username").passwordParameter("password");
 
                 })
 
                 //logout handling
-                .logout(logout->{
-                    logout
-                            .logoutSuccessUrl("/login")
-                            .logoutUrl("/logout");
+                .logout(logout -> {
+                    logout.logoutSuccessUrl("/login").logoutUrl("/logout");
                 })
 
                 //exception handling
-                .exceptionHandling(exception ->{
+                .exceptionHandling(exception -> {
                     exception.accessDeniedPage("/error");
                 })
 
                 //cross side reference
-                .csrf(csrf->{
+                .csrf(csrf -> {
                     csrf.disable();
+                })
+                //auto logout when no activity is detected
+                .sessionManagement(session -> {
+                    session.invalidSessionUrl("/login")
+                            .maximumSessions(1)
+                            .expiredUrl("/login");
                 });
 
         return httpSecurity.build();
     }
 
     @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoderMethod(){
+    public BCryptPasswordEncoder bCryptPasswordEncoderMethod() {
         bCryptPasswordEncoder = new BCryptPasswordEncoder();
         return bCryptPasswordEncoder;
     }
