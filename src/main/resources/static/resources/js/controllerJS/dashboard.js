13a540452f537b5beb9aea1d5af315618ab1baab
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-05-30 22:06:21 +0530

    comments added

--- src/main/resources/static/resources/js/controllerJS/dashboard.js
+++ src/main/resources/static/resources/js/controllerJS/dashboard.js
@@ -1,5 +1,7 @@
 window.addEventListener('load',()=>{
 
+    //using refreshDashboardWidgets to refresh the dashboard widget values
+    refreshDashboardWidgets();
 });
 
 const refreshDashboardWidgets = ()=>{
