5de571a2f78a17ee6abcab893aefd80d2b7e1eef
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-05-30 10:38:21 +0530

    edited

--- src/main/java/lk/steam/rms/controller/LoginController.java
+++ src/main/java/lk/steam/rms/controller/LoginController.java
@@ -1,11 +1,18 @@
 package lk.steam.rms.controller;
 
+import lk.steam.rms.dao.UserDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class LoginController {
+
+    @Autowired
+    private UserDAO userDAO;
 
     @GetMapping(value = "/login")
     public ModelAndView loginUI(){
@@ -19,5 +26,20 @@ public class LoginController {
         ModelAndView errorView = new ModelAndView();
         errorView.setViewName("error.html");
         return errorView;
+    }
+
+    @GetMapping(value = "/Dashboard")
+    public ModelAndView dashboardUI(){
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        ModelAndView dashboardView = new ModelAndView();
+        dashboardView.setViewName("dashboard.html");
+        dashboardView.addObject("username",auth.getName());
+        dashboardView.addObject("title","Attendance | STEAM RMS");
+        dashboardView.addObject("activeNavItem","attendance");
+        String loggedInEmployeeName = userDAO.getUserByUsername(auth.getName()).getEmployeeID().getFullName();
+        String loggedInDesignationName = userDAO.getUserByUsername(auth.getName()).getEmployeeID().getDesignationID().getDesignation();
+        dashboardView.addObject("loggedInEmployeeName",loggedInEmployeeName);
+        dashboardView.addObject("loggedInDesignationName",loggedInDesignationName);
+        return dashboardView;
     }
 }
