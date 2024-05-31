b82ff2eb777c67d7c822535c657f5002d603af10
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-05-31 10:17:33 +0530

    initial commit

---
+++ src/main/java/lk/steam/rms/controller/ReportController.java
@@ -0,0 +1,11 @@
+package lk.steam.rms.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Report")
+public class ReportController {
+
+
+}
