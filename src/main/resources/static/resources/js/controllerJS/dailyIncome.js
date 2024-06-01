5d0e84ac10b861767b9eaa01b033afb157b20d57
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-06-01 22:47:55 +0530

    major update

---
+++ src/main/resources/static/resources/js/controllerJS/dailyIncome.js
@@ -0,0 +1,53 @@
+window.addEventListener('load',()=>{
+
+
+    //daily Income calculation start
+    const dailyPayments = ajaxGetRequest("/Payment/getDailyIncome");
+    let dailyPayment=0;
+    dailyPayments.forEach((payment)=>{
+        dailyPayment += payment.amount;
+    });
+    //dailyIncomeText.innerText = "Rs. "+dailyPayment.toLocaleString('en-US',{minimumFractionDigits:2,maximumFractionDigits:2})
+    //daily Income calculation end
+
+    const displayPropertyListForDailyIncome = [
+        {property: 'addedBy', dataType: 'text'},
+        {property: getPaymentMethod, dataType: 'function'},
+        {property: getTimeStamp, dataType: 'function'},
+        {property: getRegistration, dataType: 'function'},
+        {property: getStudentName, dataType: 'function'},
+        {property: getBatch, dataType: 'function'},
+        {property: 'invoiceCode', dataType: 'text'},
+        {property: getAmount, dataType: 'function'},
+    ];
+
+    fillDataIntoTableWithOutAction(tblDailyIncome,dailyPayments,displayPropertyListForDailyIncome);
+
+    if(dailyPayments.length!==0) {
+        $('#tblDailyIncome').DataTable();
+    }
+})
+
+const getPaymentMethod = (ob)=>{
+    return ob.paymentTypeID.name;
+}
+
+const getTimeStamp = (ob)=>{
+    return ob.timeStamp.replace('T','&nbsp;&nbsp;');
+}
+
+const getRegistration = (ob)=>{
+    return ob.registrationID.registrationNumber;
+}
+
+const getStudentName = (ob)=>{
+    return ob.registrationID.studentID.nameWithInitials;
+}
+
+const getBatch = (ob)=>{
+    return ob.registrationID.courseID.name;
+}
+
+const getAmount = (ob)=>{
+    return "Rs. "+ob.amount.toLocaleString('en-US',{maximumFractionDigits: 2,minimumFractionDigits: 2})
+}
