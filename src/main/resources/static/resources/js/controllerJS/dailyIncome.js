de7d8f62ff1d5fc3ea50eb5773bbe07063df2b96
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-06-02 19:33:00 +0530

    new function added

--- src/main/resources/static/resources/js/controllerJS/dailyIncome.js
+++ src/main/resources/static/resources/js/controllerJS/dailyIncome.js
@@ -53,6 +53,18 @@ const dailyIncomeToXlsx = ()=>{
 
 }
 
+const dailyCashToXlsx = ()=>{
+
+    showCustomConfirm('You are about to export Daily Cash Income data to an Excel spreadsheet<br><br>Are You Sure?',function (result){
+        if(result){
+            exportToExcel(dailyPayments,'Daily Income Report '+reportCreatedDate,reportColumnFormat);
+            // exportTableToExcel('tblDailyIncome','test');
+        }
+    });
+
+
+}
+
 const refreshDailyBreakDownTable = ()=>{
 
 
