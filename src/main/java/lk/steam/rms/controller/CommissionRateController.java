5703cd5f38ecb8d8cd0735731e650257a3e65c1f
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-06-04 00:37:35 +0530

    service mapped

--- src/main/java/lk/steam/rms/controller/CommissionRateController.java
+++ src/main/java/lk/steam/rms/controller/CommissionRateController.java
@@ -1,9 +1,24 @@
 package lk.steam.rms.controller;
 
+import lk.steam.rms.dao.CommissionRateDAO;
+import lk.steam.rms.entity.CommissionRate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/CommissionRate")
 public class CommissionRateController {
+
+
+    @Autowired
+    private CommissionRateDAO commissionRateDAO;
+
+
+    @GetMapping("/getCommissionRateByCourseID/{courseID}")
+    public CommissionRate getCommissionRateByCourseID(@PathVariable Integer courseID){
+        return commissionRateDAO.getCommissionRateByCourseID(courseID);
+    }
 }
