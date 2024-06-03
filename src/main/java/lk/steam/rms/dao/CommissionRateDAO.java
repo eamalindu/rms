1e1cf8762d1ee9bca7f00449bc602385863c4190
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-06-04 00:33:38 +0530

    custom query added

--- src/main/java/lk/steam/rms/dao/CommissionRateDAO.java
+++ src/main/java/lk/steam/rms/dao/CommissionRateDAO.java
@@ -2,6 +2,10 @@ package lk.steam.rms.dao;
 
 import lk.steam.rms.entity.CommissionRate;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CommissionRateDAO extends JpaRepository<CommissionRate, Integer> {
+
+    @Query(value = "SELECT cr from CommissionRate cr where cr.courseID.id=?1")
+    CommissionRate getCommissionRateByCourseID(int courseID);
 }
