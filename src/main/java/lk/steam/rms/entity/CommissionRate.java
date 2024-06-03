60ffab4196563ebd620fe9e79abaf485129ec107
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-06-04 00:04:35 +0530

    edited

--- src/main/java/lk/steam/rms/entity/CommissionRate.java
+++ src/main/java/lk/steam/rms/entity/CommissionRate.java
@@ -1,4 +1,18 @@
 package lk.steam.rms.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "commsionrate")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CommissionRate {
+    @Id
+    @Column(name = "id",unique = true)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 }
