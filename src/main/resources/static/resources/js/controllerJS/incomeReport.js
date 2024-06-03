dc8f33a213fefa3f16fdf37da8aef9e72eef70cc
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-06-03 13:33:11 +0530

    major update

---
+++ src/main/resources/static/resources/js/controllerJS/incomeReport.js
@@ -0,0 +1,27 @@
+window.addEventListener('load',()=>{
+    var start = moment().subtract(29, 'days');
+    var end = moment();
+
+    function cb(start, end) {
+        $('#registrationSearchDateRange span').html(start.format('YYYY-MMMM-DD') + ' - ' + end.format('YYYY-MMMM-DD'));
+    }
+
+    $('#registrationSearchDateRange').daterangepicker({
+        startDate: start,
+        endDate: end,
+        locale: {
+            "format": "YYYY-MM-DD",
+        },
+        ranges: {
+            'Today': [moment(), moment()],
+            'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+            'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+            'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+            'This Month': [moment().startOf('month'), moment().endOf('month')],
+            'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+        }
+    }, cb);
+
+    cb(start, end);
+
+})
