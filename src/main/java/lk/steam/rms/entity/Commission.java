7b1f76048e5b215075bc9694e5899b2f70e9fb53
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-06-03 23:41:20 +0530

    completed

--- src/main/java/lk/steam/rms/entity/Commission.java
+++ src/main/java/lk/steam/rms/entity/Commission.java
@@ -1,9 +1,12 @@
 package lk.steam.rms.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
 
 @Entity
 @Table(name = "commission")
@@ -17,5 +20,15 @@ public class Commission {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @Column(name = "inquiryid")
+    @NotNull
+    private Integer inquiryID;
 
+    @Column(name = "amount")
+    @NotNull
+    private BigDecimal amount;
+
+    @Column(name = "paidto")
+    @NotNull
+    private String paidTo;
 }
