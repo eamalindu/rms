08a17c439f3f257f504ed0d5c3951d5aa20b8568
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-06-03 23:39:04 +0530

    edited

--- src/main/java/lk/steam/rms/entity/Commission.java
+++ src/main/java/lk/steam/rms/entity/Commission.java
@@ -1,4 +1,21 @@
 package lk.steam.rms.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "commission")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Commission {
+
+    @Id
+    @Column(name = "id",unique = true)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+
 }
