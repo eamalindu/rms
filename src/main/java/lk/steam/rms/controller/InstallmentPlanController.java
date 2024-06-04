fd53707c67b804fdd2b7a5fbc59bb5e7bcf2a6ab
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-06-04 18:48:29 +0530

    privilege finalize

--- src/main/java/lk/steam/rms/controller/InstallmentPlanController.java
+++ src/main/java/lk/steam/rms/controller/InstallmentPlanController.java
@@ -38,7 +38,7 @@ public class InstallmentPlanController {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         Privilege loggedUserPrivilege = privilegeController.getPrivilegeByUserAndModule(auth.getName(),"BATCH");
 
-        if(!loggedUserPrivilege.getDeletePrivilege()){
+        if(!loggedUserPrivilege.getInsertPrivilege()){
             return "<br>User does not have sufficient privilege.";
         }
 
