920b33949d6b654b6b29c592f59596d7d2584508
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-06-04 14:28:06 +0530

    edited

--- src/main/java/lk/steam/rms/dao/CommissionDAO.java
+++ src/main/java/lk/steam/rms/dao/CommissionDAO.java
@@ -2,6 +2,10 @@ package lk.steam.rms.dao;
 
 import lk.steam.rms.entity.Commission;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CommissionDAO extends JpaRepository<Commission, Integer> {
+
+    @Query("select c from Commission c where c.registrationID.id=?1")
+    Commission findCommissionByRegistrationID(Integer registrationID);
 }
