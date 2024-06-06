b93d4fb118a3914340da787a9862510c2caf46d1
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-06-06 18:35:19 +0530

    edited

--- src/main/resources/static/resources/js/controllerJS/resetPassword.js
+++ src/main/resources/static/resources/js/controllerJS/resetPassword.js
@@ -3,17 +3,14 @@ window.addEventListener("load", () => {
     btnSendOtp.disabled = true;
 
     username.addEventListener('keyup', () => {
-        if(username.value!==''){
+        if (username.value !== '') {
             if (/^[a-zA-Z]{2,19}@[a-zA-Z]{2,8}\.[a-zA-Z]{2,3}$/.test(username.value)) {
                 btnSendOtp.disabled = false;
-            }
-
-            else{
+            } else {
                 btnSendOtp.disabled = true;
             }
 
-        }
-        else{
+        } else {
             btnSendOtp.disabled = true;
         }
 
@@ -23,6 +20,29 @@ window.addEventListener("load", () => {
 
 
 const sendOTP = () => {
+    const serverResponse = ajaxHttpRequest("/Reset-Password/" + username.value, "POST");
+    if (serverResponse === "OK") {
+        lblPassword.classList.remove('d-none');
+        countdown(2, countdownText, 'You can request another OTP after');
+
+        btnSendOtp.disabled = true;
+        username.disabled = true;
+        btnLogin.disabled = false;
+
+        otpContainer.classList.remove('d-none');
+        //check an account is present
+        //send a email
+        //display a user msg
+        password.disabled = false;
+        password.focus();
+        setTimeout(function () {
+            btnSendOtp.disabled = false;
+            username.disabled = false;
+            countdownText.innerHTML = '<small>You Can Request a New OTP Now</small>'
+        }, 120000);
+    } else {
+        showCustomModal(serverResponse, "error");
+    }
     //
     // const User = ajaxGetRequest("Reset-Password/getUserByEmail/"+username.value)
     // if(User!==''){
@@ -61,19 +81,21 @@ const sendOTP = () => {
 function countdown(minutes) {
     var seconds = 60;
     var mins = minutes
+
     function tick() {
         //This script expects an element with an ID = "counter". You can change that to what ever you want.
         var counter = document.getElementById("countdownText");
-        var current_minutes = mins-1
+        var current_minutes = mins - 1
         seconds--;
-        counter.innerHTML ="<small>You can request another OTP after 0"+ current_minutes.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds)+" mintues</small>";
-        if( seconds > 0 ) {
+        counter.innerHTML = "<small>You can request another OTP after 0" + current_minutes.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds) + " mintues</small>";
+        if (seconds > 0) {
             setTimeout(tick, 1000);
         } else {
-            if(mins > 1){
-                countdown(mins-1);
+            if (mins > 1) {
+                countdown(mins - 1);
             }
         }
     }
+
     tick();
 }
