d55fc51821f0b6611e31c8955a921b5927efcf56
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-06-06 16:46:13 +0530

    html email added

--- src/main/java/lk/steam/rms/service/MailService.java
+++ src/main/java/lk/steam/rms/service/MailService.java
@@ -35,7 +35,82 @@ public class MailService {
         helper.setFrom(username);
         helper.setTo(mail);
         helper.setSubject("OTP");
-        String msgContent = "";
+        String msgContent = "<!DOCTYPE html>" +
+                "<html lang='en'>" +
+                "<head>" +
+                "    <meta charset='UTF-8'>" +
+                "    <meta name='viewport' content='width=device-width, initial-scale=1.0'>" +
+                "    <title>OTP Email</title>" +
+                "    <style>" +
+                "        @import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');" +
+                "        body {" +
+                "            font-family: 'Poppins', sans-serif;" +
+                "            background-color: #f4f4f4;" +
+                "            margin: 0;" +
+                "            padding: 0;" +
+                "            display:flex;" +
+                "            width:100%;" +
+                "            height:100vh;" +
+                "            align-items:center;" +
+                "            justify-content:center;" +
+                "        }" +
+                "        .container {" +
+                "            width: 100%;" +
+                "            max-width: 550px;" +
+                "            margin: 0 auto;" +
+                "            background-color: #ffffff;" +
+                "            border-radius: 8px;" +
+                "            overflow: hidden;" +
+                "            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);" +
+                "        }" +
+                "        .header {" +
+                "           margin-left: 20px;" +
+                "            border-bottom:1px solid #f1f1f1 ;" +
+                "        }" +
+                "    " +
+                "        .content {" +
+                "            padding: 20px;" +
+                "        }" +
+                "        .otp {" +
+                "            display: block;" +
+                "            width: fit-content;" +
+                "            font-size: 24px;" +
+                "            font-weight: bold;" +
+                "            margin-top: 5px;" +
+                "            margin-bottom: 5px;" +
+                "                " +
+                "        }" +
+                "        .footer {" +
+                "            background-color: #f4f4f4;" +
+                "            padding: 10px;" +
+                "            text-align: center;" +
+                "            color: #777777;" +
+                "            font-size: 14px;" +
+                "        }" +
+                "        .footer p {" +
+                "            margin: 5px 0;" +
+                "        }" +
+                "    </style>" +
+                "</head>" +
+                "<body>" +
+                "    <div class='container'>" +
+                "        <div class='header'>" +
+                "            <h2>Password Assistance</h2>" +
+                "        </div>" +
+                "        <div class='content'>" +
+                "            We received a request to reset your password. &nbsp;Your One-Time Password (OTP) for password reset is : " +
+                "            <div class='otp'>123456</div>" +
+                "            Please enter this OTP to reset your password. &nbsp;This code is valid for the next 5 minutes." +
+                "            <br/><br/>" +
+                "            If you did not request a password reset, please ignore this email." +
+                "           " +
+                "        </div>" +
+                "        <div class='footer'>" +
+                "            <p>We Hope to see you again</p>" +
+                "        </div>" +
+                "    </div>" +
+                "</body>" +
+                "</html>";
         helper.setText(msgContent, true); // true indicates HTML
 
         javaMailSender.send(message);
