7c661c6eadf725cf71aa03bcac77f78243ab4140
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-06-06 20:01:23 +0530

    edited

--- src/main/java/lk/steam/rms/service/MailService.java
+++ src/main/java/lk/steam/rms/service/MailService.java
@@ -38,7 +38,7 @@ public class MailService {
 
         helper.setFrom(username);
         helper.setTo(mail);
-        helper.setSubject("Password Reset [OTP Confirmation]");
+        helper.setSubject("Password Reset 🔑 [OTP Confirmation]");
         String msgContent = "<!DOCTYPE html>" +
                 "<html lang='en'>" +
                 "<head>" +
