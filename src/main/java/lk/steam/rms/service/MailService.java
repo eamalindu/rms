7c7d95531ca9e74d48c24605d86121805bc4e210
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-06-06 16:19:06 +0530

    new service

--- src/main/java/lk/steam/rms/service/MailService.java
+++ src/main/java/lk/steam/rms/service/MailService.java
@@ -1,10 +1,14 @@
 package lk.steam.rms.service;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import lk.steam.rms.entity.MailStructure;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMailMessage;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -22,5 +26,19 @@ public class MailService {
         message.setText(mailStructure.getMessage());
         message.setTo(mail);
         javaMailSender.send(message);
+    }
+
+    public void sendOTPMail(String mail) throws MessagingException {
+        MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message,true,"UTF-8");
+
+        helper.setFrom(username);
+        helper.setTo(mail);
+        helper.setSubject("OTP");
+        String msgContent = "";
+        helper.setText(msgContent, true); // true indicates HTML
+
+        javaMailSender.send(message);
+
     }
 }
