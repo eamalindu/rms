e4d42b4c0819cb69ab29f3b1eda7195c20e050c3
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-06-06 20:42:13 +0530

    custom query added

--- src/main/java/lk/steam/rms/dao/OtpDAO.java
+++ src/main/java/lk/steam/rms/dao/OtpDAO.java
@@ -2,6 +2,11 @@ package lk.steam.rms.dao;
 
 import lk.steam.rms.entity.OTP;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 
 public interface OtpDAO extends JpaRepository<OTP, Integer> {
+
+    @Query(value = "SELECT otp from OTP otp where otp.email=?1 and otp.otp=?2 and otp.expiredTimestamp<=now() and otp.status=1")
+    OTP getOTPByEmailAndOtp(String email, String otp);
 }
