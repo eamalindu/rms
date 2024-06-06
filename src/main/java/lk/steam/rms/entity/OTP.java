7cc091bd97876e0487adc690a5a5c82147febe71
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-06-06 20:17:20 +0530

    edited

--- src/main/java/lk/steam/rms/entity/OTP.java
+++ src/main/java/lk/steam/rms/entity/OTP.java
@@ -1,6 +1,7 @@
 package lk.steam.rms.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,17 +16,28 @@ import java.time.LocalDateTime;
 public class OTP {
 
     @Id
+    @Column(name = "id",unique = true)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @Column(name = "email")
+    @NotNull
     private String email;
 
+    @Column(name = "otp")
+    @NotNull
     private String otp;
 
+    @Column(name = "createdtimestamp")
+    @NotNull
     private LocalDateTime createdTimestamp;
 
+    @Column(name = "expiredtimestamp")
+    @NotNull
     private LocalDateTime expiredTimestamp;
 
+    @Column(name = "status")
+    @NotNull
     private Boolean status;
 
     @ManyToOne
