e8fa94040bd0f5c11dd26baba3dd21a691622f23
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-06-06 19:10:05 +0530

    edited

--- src/main/java/lk/steam/rms/service/MailService.java
+++ src/main/java/lk/steam/rms/service/MailService.java
@@ -7,8 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMailMessage;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Service;
 
@@ -21,6 +21,7 @@ public class MailService {
     @Value("${spring.mail.username}")
     private String username;
 
+    @Async
     public void sendMail(String mail, MailStructure mailStructure) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setFrom(username);
@@ -30,6 +31,7 @@ public class MailService {
         javaMailSender.send(message);
     }
 
+    @Async
     public void sendOTPMail(String mail,String otp) throws MessagingException {
         MimeMessage message = javaMailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(message,true,"UTF-8");
