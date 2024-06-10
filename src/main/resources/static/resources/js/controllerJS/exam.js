e466a320ffb4bec37963dd5a696eac8280494b08
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-06-10 18:46:56 +0530

    edited

--- src/main/resources/static/resources/js/controllerJS/exam.js
+++ src/main/resources/static/resources/js/controllerJS/exam.js
@@ -1,8 +1,22 @@
 window.addEventListener('load',()=>{
-    $('#examSearchCourse').chosen({width: '200px'});
-    $('#examSearchBatch').chosen({width: '200px'});
-    $('#examSearchLesson').chosen({width: '200px'});
-    $('#examSearchRegistration').chosen({width: '200px'});
+    resetSearchBar();
+    $("#examSearchCourse").chosen().change(function () {
+        $("#examSearchCourse_chosen .chosen-single").addClass('bg-light');
+    });
+    $("#examSearchBatch").chosen().change(function () {
+        $("#examSearchBatch_chosen .chosen-single").addClass('bg-light');
+    });
+    $("#examSearchLesson").chosen().change(function () {
+        $("#examSearchLesson_chosen .chosen-single").addClass('bg-light');
+    });
+    $("#examSearchRegistration").chosen().change(function () {
+        $("#examSearchRegistration_chosen .chosen-single").addClass('bg-light');
+    });
+
+
+})
+
+const resetSearchBar=()=>{
 
     //initialize 3rd party daterangepicker library
     //Set the minDate for the batchSheetCommenceDate as the current object's commenceDate value
@@ -17,8 +31,19 @@ window.addEventListener('load',()=>{
             "format": "YYYY-MM-DD"
         }
     });
-})
 
-const resetSearchBar=()=>{
+    const courses = ajaxGetRequest("/Course/findall");
+    fillSelectOptions(examSearchCourse, ' ', courses, 'name');
 
+
+    $('#examSearchCourse').chosen({width: '200px'});
+    $('#examSearchBatch').chosen({width: '200px'});
+    $('#examSearchLesson').chosen({width: '200px'});
+    $('#examSearchRegistration').chosen({width: '200px'});
+}
+
+const getBatches = ()=>{
+    const batches = ajaxGetRequest("/Batch/getBatchesByCourseID/" + JSON.parse(examSearchCourse.value).id)
+    fillSelectOptions(examSearchBatch, ' ', batches, 'batchCode')
+    $('#examSearchBatch').val('').trigger('chosen:updated');
 }
