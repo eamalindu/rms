c944a9929b94aafd93b88ccdde17fadbb580b382
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-06-10 16:44:29 +0530

    initial commit

---
+++ src/main/java/lk/steam/rms/controller/ExamAttempt.java
@@ -0,0 +1,10 @@
+package lk.steam.rms.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Exam")
+public class ExamAttempt {
+
+}
