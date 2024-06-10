6dacc68186f89d8b51f8c399b6aba58de0501261
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-06-10 21:27:50 +0530

    custom query added

--- src/main/java/lk/steam/rms/dao/ExamAttemptDAO.java
+++ src/main/java/lk/steam/rms/dao/ExamAttemptDAO.java
@@ -2,6 +2,12 @@ package lk.steam.rms.dao;
 
 import lk.steam.rms.entity.ExamAttempt;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ExamAttemptDAO extends JpaRepository<ExamAttempt, Integer> {
+
+    @Query(value = "SELECT ea from ExamAttempt ea where ea.examDate>=current_date()")
+    List<ExamAttempt> findActiveExamAttempts();
 }
