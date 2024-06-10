e7246fa46d72fb1e4787773f4e71aa06e53a24a4
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-06-10 18:28:04 +0530

    edited

--- src/main/resources/static/resources/js/controllerJS/exam.js
+++ src/main/resources/static/resources/js/controllerJS/exam.js
@@ -3,4 +3,18 @@ window.addEventListener('load',()=>{
     $('#examSearchBatch').chosen({width: '200px'});
     $('#examSearchLesson').chosen({width: '200px'});
     $('#examSearchRegistration').chosen({width: '200px'});
+
+    //initialize 3rd party daterangepicker library
+    //Set the minDate for the batchSheetCommenceDate as the current object's commenceDate value
+    $('#examSearchDateRange').daterangepicker({
+        "minDate": new Date(),
+        "singleDatePicker": true,
+        "linkedCalendars": false,
+        "showCustomRangeLabel": false,
+        "autoUpdateInput": false,
+        "drops": "down",
+        "locale": {
+            "format": "YYYY-MM-DD"
+        }
+    });
 })
