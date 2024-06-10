b8d6e4a5f8a25dc831836476b93ef5ab4a26038a
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-06-10 15:02:37 +0530

    edited

--- src/main/java/lk/steam/rms/entity/Batch.java
+++ src/main/java/lk/steam/rms/entity/Batch.java
@@ -84,7 +84,7 @@ public class Batch {
     @OneToMany(mappedBy = "batchID",cascade = CascadeType.ALL,orphanRemoval = true)
     private List<BatchHasDay> batchHasDayList;
 
-    @OneToMany(mappedBy = "",cascade = CascadeType.ALL,orphanRemoval = true)
+    @OneToMany(mappedBy = "batchID",cascade = CascadeType.ALL,orphanRemoval = true)
     private List<BatchHasLesson> batchHasLessonList;
 
 }
