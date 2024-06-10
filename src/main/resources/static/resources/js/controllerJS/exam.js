cda1505543ba8df0fda19246d102e96afee839b2
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-06-10 18:18:49 +0530

    edited

---
+++ src/main/resources/static/resources/js/controllerJS/exam.js
@@ -0,0 +1,6 @@
+window.addEventListener('load',()=>{
+    $('#examSearchCourse').chosen({width: '200px'});
+    $('#examSearchBatch').chosen({width: '200px'});
+    $('#examSearchLesson').chosen({width: '200px'});
+    $('#examSearchRegistration').chosen({width: '200px'});
+})
