9083c38e8d7d1fe43dbd07251d052a85c4847872
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-06-12 22:49:21 +0530

    autowired

--- src/main/java/lk/steam/rms/controller/MarksValidationController.java
+++ src/main/java/lk/steam/rms/controller/MarksValidationController.java
@@ -1,5 +1,7 @@
 package lk.steam.rms.controller;
 
+import lk.steam.rms.dao.UserDAO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +12,10 @@ import org.springframework.web.servlet.ModelAndView;
 @RestController
 @RequestMapping("/Marks-Validation")
 public class MarksValidationController {
+    @Autowired
+    private PrivilegeController privilegeController;
+    @Autowired
+    private UserDAO userDAO;
 
     @GetMapping()
     public ModelAndView marksUI() {
