d7c68ff3ddd1577fb876a40acdcdede647692834
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-06-18 13:40:26 +0530

    new function added

--- src/main/resources/static/resources/js/controllerJS/dayPlan.js
+++ src/main/resources/static/resources/js/controllerJS/dayPlan.js
@@ -1,5 +1,10 @@
 window.addEventListener("load", () => {
+
+    resetDayPlanForm();
+});
+
+const resetDayPlanForm = () => {
     const batches = ajaxGetRequest("/Batch/getBatchesConductTodayByLecturer/" + btnProfileName.innerText);
     fillSelectOptions(dayPlanBatch, ' ', batches, "batchCode");
     $('#dayPlanBatch').chosen({width: '100%'});
-});
+}
