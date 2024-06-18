dc3937017831c6ebf34e95947597ff7bf60c295d
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-06-18 14:23:11 +0530

    major update

--- src/main/resources/static/resources/js/controllerJS/dayPlan.js
+++ src/main/resources/static/resources/js/controllerJS/dayPlan.js
@@ -27,4 +27,32 @@ const resetInnerForm = ()=>{
 const getBatchInfo = ()=>{
     const selectedBatch = JSON.parse(dayPlanBatch.value);
     const attendances  = ajaxGetRequest("/Attendance/getAttendanceByBatchIDForToday/"+selectedBatch.id)
+
+    //set information
+    collapseBatchAttendance.innerText = attendances.length;
+
+    collapseBatchDuration.innerText =   selectedBatch.batchHasDayList;
+
+    //test code
+    // Create a new Date object for the current date and time
+    const currentDate = new Date();
+
+// Define an array with the full names of the days of the week
+    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+// Get the current day of the week as a number (0-6) using getDay()
+    const currentDayNumber = currentDate.getDay();
+
+// Use the day number to get the corresponding day name from the array
+    const currentDayName = daysOfWeek[currentDayNumber];
+
+    for(let i = 0; i < selectedBatch.batchHasDayList.length; i++){
+        const day = selectedBatch.batchHasDayList[i];
+        if(day.dayID.name === currentDayName){
+            collapseBatchStartTime.innerText = day.startTime;
+            collapseBatchEndTime.innerText = day.endTime;
+            break;
+        }
+    }
+
 }
