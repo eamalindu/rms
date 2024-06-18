87521157118fdc42ab1ce917ab2230ce35be0543
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-06-18 12:33:48 +0530

    properties added

--- src/main/java/lk/steam/rms/entity/DayPlan.java
+++ src/main/java/lk/steam/rms/entity/DayPlan.java
@@ -1,4 +1,32 @@
 package lk.steam.rms.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "dayplan")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class DayPlan {
+
+    @Id
+    @Column(name = "id",unique = true)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private LocalTime startTime;
+
+    private LocalTime endTime;
+
+    private String addedBy;
+
+    private LocalDateTime timeStamp;
+
+    //lesson id here
 }
