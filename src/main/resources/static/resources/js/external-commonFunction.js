b6cff4806bd3465f6c0b8a2b239673bbef18db7b
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-06-25 22:08:01 +0530

    w-auto class added

--- src/main/resources/static/resources/js/external-commonFunction.js
+++ src/main/resources/static/resources/js/external-commonFunction.js
@@ -239,7 +239,7 @@ const createCourseRadioCards = (dataList, functionEx, container) => {
         cardDiv.innerHTML = `
           <div class="card-body p-1">
             <div class="d-flex">
-            <div class="w-25">
+            <div class="w-auto">
                 <img src="data:image/png;base64,${element.logo}" width="100px">
             </div>
             <div class="w-100 d-flex align-items-center justify-content-center">
