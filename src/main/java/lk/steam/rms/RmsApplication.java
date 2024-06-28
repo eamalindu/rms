060cf4d9bf418c0d4a0467331d456666be7075dc
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-06-28 12:31:33 +0530

    logged in image added

--- src/main/java/lk/steam/rms/RmsApplication.java
+++ src/main/java/lk/steam/rms/RmsApplication.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.Base64;
+
 @SpringBootApplication
 @RestController
 public class RmsApplication {
@@ -34,8 +36,12 @@ public class RmsApplication {
 		newRegistrationView.addObject("activeNavItem","newRegistration");
 		String loggedInEmployeeName = userDAO.getUserByUsername(auth.getName()).getEmployeeID().getFullName();
 		String loggedInDesignationName = userDAO.getUserByUsername(auth.getName()).getEmployeeID().getDesignationID().getDesignation();
+		byte[] photoBytes = userDAO.getUserByUsername(auth.getName()).getEmployeeID().getPhotoPath();
+		String base64Image = Base64.getEncoder().encodeToString(photoBytes);
+		String imageSrc = "data:image/png;base64," + base64Image;
 		newRegistrationView.addObject("loggedInEmployeeName",loggedInEmployeeName);
 		newRegistrationView.addObject("loggedInDesignationName",loggedInDesignationName);
+		newRegistrationView.addObject("loggedInImage",imageSrc);
 
 		return newRegistrationView;
 	}
