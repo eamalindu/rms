02648af4007a30855894dae8d0000be55c8da0f9
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-06-30 17:06:18 +0530

    formatted

--- src/main/resources/static/resources/js/external-chart.js
+++ src/main/resources/static/resources/js/external-chart.js
@@ -110,7 +110,7 @@ const generateLineChart = (elementID, title, categories, yAxis, series) => {
         tooltip: {
             headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
             pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-                '<td style="padding:0"><b>{point.y} {yAxis}</b></td></tr>',
+                '<td style="padding:0"><b>&nbsp;Rs.{point.y} {yAxis}</b></td></tr>',
             footerFormat: '</table>',
             shared: true,
             useHTML: true
