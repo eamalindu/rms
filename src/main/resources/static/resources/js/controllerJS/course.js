9e6d444a5163a2c09455a8d3ad196663dc39958a
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-07-15 19:34:04 +0530

    new function defined

--- src/main/resources/static/resources/js/controllerJS/course.js
+++ src/main/resources/static/resources/js/controllerJS/course.js
@@ -367,3 +367,26 @@ const checkCourseFormErrors =(courseObject)=>{
 
     return errors;
 }
+
+const courseEdit = ()=>{
+    //getting the toast from its ID
+    var myToastEl = document.getElementById('myToast');
+    var myToast = new bootstrap.Toast(myToastEl);
+    //Displaying toast
+    myToast.show();
+    //hide the toast after 5s
+    setTimeout(function () {
+        myToast.hide();
+    }, 5000);
+
+    //remove the attribute readonly to make inputs accept the user input values
+    //give a border color to inputs indicate that the input's values are ready to be edited
+    inputs = document.querySelectorAll('.courseSheetInputs');
+
+    //remove the disabled attribute from the select
+    //give a border color to indicate that select can be now edited
+    inputs.forEach(function (input) {
+        input.removeAttribute('disabled');
+        input.setAttribute('style', 'border:1px solid #0DCAF0!important;background-color:rgba(13,202,240,0.2);');
+    });
+}
