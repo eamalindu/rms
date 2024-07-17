5c10c47655ee9d81d7631c3d99e834057f3f2ce1
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-07-17 14:14:41 +0530

    reformatted

--- src/main/java/lk/steam/rms/entity/Inquiry.java
+++ src/main/java/lk/steam/rms/entity/Inquiry.java
@@ -10,44 +10,44 @@ import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "inquiry")
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Inquiry {
 
+    //Primary key mapping with auto increment
     @Id
-    @Column(name = "id",unique = true)
+    @Column(name = "id", unique = true)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(name = "inquirynumber",unique = true)
+    @Column(name = "inquirynumber", unique = true)
     @NotNull
     private String inquiryNumber;
 
-    @Column(name = "firstname",length = 20)
+    @Column(name = "firstname", length = 20)
     @NotNull
     private String firstName;
 
-    @Column(name = "lastname",length = 30)
+    @Column(name = "lastname", length = 30)
     @NotNull
     private String lastName;
 
-    @Column(name = "primarymobilenumber",length = 10)
+    @Column(name = "primarymobilenumber", length = 10)
     @NotNull
     private String primaryMobileNumber;
 
-    @Column(name = "secondarymobilenumber",length = 10)
+    @Column(name = "secondarymobilenumber", length = 10)
     private String secondaryMobileNumber;
 
     @Column(name = "email")
     private String email;
 
-    @Column(name = "idtype",length = 6)
+    @Column(name = "idtype", length = 6)
     @NotNull
     private String idType;
 
-    @Column(name = "idvalue",length = 15)
+    @Column(name = "idvalue", length = 15)
     @NotNull
     private String idValue;
 
@@ -59,7 +59,7 @@ public class Inquiry {
     @NotNull
     private String description;
 
-    @Column(name = "addedby",length = 30)
+    @Column(name = "addedby", length = 30)
     @NotNull
     private String addedBy;
 
@@ -73,16 +73,17 @@ public class Inquiry {
     @Column(name = "registrationid")
     private Integer registrationID;
 
+    //Foreign keys mapping
     @ManyToOne
-    @JoinColumn(name = "source_id",referencedColumnName = "id")
+    @JoinColumn(name = "source_id", referencedColumnName = "id")
     private Source sourceId;
 
     @ManyToOne
-    @JoinColumn(name = "inquirystatus_id",referencedColumnName = "id")
+    @JoinColumn(name = "inquirystatus_id", referencedColumnName = "id")
     private InquiryStatus inquiryStatusId;
 
     @ManyToOne
-    @JoinColumn(name = "course_id",referencedColumnName = "id")
+    @JoinColumn(name = "course_id", referencedColumnName = "id")
     private Course courseId;
 
 }
