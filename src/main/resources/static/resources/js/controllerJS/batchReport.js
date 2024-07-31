e3daf6e14b0ac1ca520c65a2bd94c8586b8af8a0
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-07-31 17:01:39 +0530

    edited

--- src/main/resources/static/resources/js/controllerJS/batchReport.js
+++ src/main/resources/static/resources/js/controllerJS/batchReport.js
@@ -5,5 +5,15 @@ window.addEventListener('load',()=>{
 
 const resetSearchBar = ()=>{
     const courses = ajaxGetRequest("/Course/findall");
+    fillSelectOptions(SearchCourse,' ',courses,'name');
+
+    $('#SearchCourse').chosen();
+    $('#SearchBatch').chosen();
+}
+
+const getBatches = ()=>{
+    selectedCourse  = JSON.parse(SearchCourse.value);
+    const batches = ajaxGetRequest("/Batch/getBatchesByCourseID/"+selectedCourse.id);
+    fillSelectOptions(SearchBatch,' ',batches,'batchCode');
 
 }
