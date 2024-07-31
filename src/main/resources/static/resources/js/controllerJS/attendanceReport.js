6c305e64165167bba34dd36496cb4b0b4bd5a230
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-07-31 19:42:59 +0530

    edited

---
+++ src/main/resources/static/resources/js/controllerJS/attendanceReport.js
@@ -0,0 +1,19 @@
+window.addEventListener('load',()=>{
+
+    resetSearchBar()
+})
+
+const resetSearchBar = () => {
+    //initialize 3rd party daterangepicker library
+    //Set the minDate for the batchSheetCommenceDate as the current object's commenceDate value
+    $('#searchDateRange').daterangepicker({
+        "singleDatePicker": true,
+        "linkedCalendars": false,
+        "showCustomRangeLabel": false,
+        "autoUpdateInput": true,
+        "drops": "down",
+        "locale": {
+            "format": "YYYY-MM-DD"
+        }
+    });
+}
