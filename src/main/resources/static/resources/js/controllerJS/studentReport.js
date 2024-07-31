688aed73f7f09050e68fb59823557b28b95a9ef0
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-07-31 16:13:27 +0530

    edited

--- src/main/resources/static/resources/js/controllerJS/studentReport.js
+++ src/main/resources/static/resources/js/controllerJS/studentReport.js
@@ -1,10 +1,32 @@
 window.addEventListener('load',()=>{
-
+    resetSearchBar();
+    $('#registrationSearchDateRange').on('apply.daterangepicker', function(ev, picker) {
+        $(this).val(picker.startDate.format('YYYY-MM-DD') + ' - ' + picker.endDate.format('YYYY-MM-DD'));
+    });
 });
 
 
 const resetSearchBar = ()=>{
+    function cb(start, end) {
+        $('#registrationSearchDateRange span').html(start.format('YYYY-MMMM-DD') + ' - ' + end.format('YYYY-MMMM-DD'));
+    }
 
+    $('#registrationSearchDateRange').daterangepicker({
+        startDate: start, endDate: end,
+        locale: {
+            "format": "YYYY-MM-DD",
+        }, ranges: {
+            'Today': [moment(), moment()],
+            'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+            'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+            'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+            'This Month': [moment().startOf('month'), moment().endOf('month')],
+            'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')],
+            'Last Year': [moment().subtract(1, 'year').startOf('year'), moment().subtract(1, 'year').endOf('year')]
+        }
+    }, cb);
+
+    cb(start, end);
 }
 
 const getStudentReport = ()=>{
