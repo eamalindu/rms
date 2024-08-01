2fb390e054cd3dce79f869571288b96508a683c6
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-08-01 14:08:07 +0530

    edited

--- src/main/resources/static/resources/js/controllerJS/exam.js
+++ src/main/resources/static/resources/js/controllerJS/exam.js
@@ -8,21 +8,21 @@ const resetMarkForm = () => {
 }
 
 const refreshMarkTable = () => {
-    //getting current Course from the database using ajaxGetRequest function and assign the response to the variable batches
+    //getting current Marks from the database using ajaxGetRequest function and assign the response to the variable batches
     marks = ajaxGetRequest("/Mark/findall");
-    //creating a display property list for the Course
+    //creating a display property list for the Mark
     displayPropertyListForMark = [{property: 'name', dataType: 'text'},
         {property: 'code', dataType: 'text'},
         {property: getDuration, dataType: 'function'},
         {property: 'minimumRequirement', dataType: 'text'},
         {property: 'lectureHours', dataType: 'text'},
         {property: getStatus, dataType: 'function'},]
-    //using external function fillDataIntoTable to fill the data to the table tblCourse according to the displayPropertyListForCourse list
+    //using external function fillDataIntoTable to fill the data to the table tblExamMarks according to the displayPropertyListForMark list
     fillDataIntoTable(tblExamMarks, marks, displayPropertyListForMark, rowView, 'offcanvasMarkSheet');
 
-    //check the length of the courses array
+    //check the length of the marks array
     if (marks.length !== 0) {
-        //initializing DataTable for the tblBatch table
+        //initializing DataTable for the tblExamMarks table
         $('#tblExamMarks').DataTable();
     }
 
