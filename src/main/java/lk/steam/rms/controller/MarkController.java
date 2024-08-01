d8f8ba6a39d439f72185f455471ec7b2b49bd7ea
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-08-01 13:56:59 +0530

    edited

--- src/main/java/lk/steam/rms/controller/MarkController.java
+++ src/main/java/lk/steam/rms/controller/MarkController.java
@@ -25,27 +25,37 @@ public class MarkController {
         return markDAO.findAll();
     }
 
+    // Save new mark
     @PostMapping
     public String saveNewMark(@RequestBody Mark mark) {
+        // Get the logged user's privilege
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         Privilege loggedUserPrivilege = privilegeController.getPrivilegeByUserAndModule(auth.getName(), "MARK");
 
+        // Check if the logged user has the privilege to insert a new mark
         if (!loggedUserPrivilege.getInsertPrivilege()) {
             return "<br>User does not have sufficient privilege.";
         }
-
+        // Check if the mark already exists
         Mark existMark = markDAO.getMarkByRegistrationIDAndLessonID(mark.getRegistrationID().getId(), mark.getLessonID().getId());
 
+        // If the mark already exists, return an error message
         if (existMark != null) {
             return "<br>Mark already exists.";
         }
+
         try {
+            // Save the new mark
+            // Set the added by, is verified and timestamp
             mark.setAddedBy(auth.getName());
             mark.setIsVerified(false);
             mark.setTimeStamp(LocalDateTime.now());
             markDAO.save(mark);
             return "OK";
-        } catch (Exception ex) {
+
+        }
+        // If an exception occurs, return an error message
+        catch (Exception ex) {
             return "Save Failed " + ex.getMessage();
         }
 
