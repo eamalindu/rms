153fe614b9aa99c46c25097bdc68a27b002dd259
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-08-01 13:25:56 +0530

    edited

---
+++ src/main/java/lk/steam/rms/entity/Mark.java
@@ -0,0 +1,42 @@
+package lk.steam.rms.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "marks")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Mark {
+    @Id
+    @Column(name = "id",unique = true)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "marks")
+    private Integer marks;
+
+    @Column(name = "addedby")
+    private String addedBy;
+
+    @Column(name = "isverfied")
+    private Boolean isVerified;
+
+    //foreign keys mapping
+    @ManyToOne
+    @JoinColumn(name = "registration_id",referencedColumnName = "id")
+    private Registrations registrationID;
+
+    @ManyToOne
+    @JoinColumn(name = "lesson_id",referencedColumnName = "id")
+    private Lesson lessonID;
+
+    @ManyToOne
+    @JoinColumn(name = "batch_id",referencedColumnName = "id")
+    private Batch batchID;
+
+
+}
