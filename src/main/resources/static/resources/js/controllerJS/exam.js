d2c94e99d5658aaf9b5840f33adf76d9676db9f4
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-08-01 16:13:14 +0530

    edited

--- src/main/resources/static/resources/js/controllerJS/exam.js
+++ src/main/resources/static/resources/js/controllerJS/exam.js
@@ -130,5 +130,56 @@ const getLessons = () => {
 }
 
 const newExamMarkSubmit = () => {
+    console.log(newExamMark);
+    //calling the checkBatchFormErrors function and catching the return value to errors variable
+    let errors = checkMarkFormErrors(newExamMark);
+    //check the errors variable is null
+    //if it's empty that means all the required inputs are filled
+    if (errors === '') {
+        //get a user confirmation using external customConfirm js
+        showCustomConfirm("You are about to add a New Mark<br>Are You Sure?", function (result) {
+            if (result) {
+                //if the user confirmation is "yes" call the ajaxHttpRequest to pass the data to backend via ajax
+                //catch the return value from the backend and save it in the serviceResponse variable
+                let serviceResponse = ajaxHttpRequest("/Mark", 'POST', newExamMark);
+                //check the serviceResponse value is "OK"
+                if (serviceResponse === "OK") {
+                    //this means data successfully passed to the backend
+                    //show an alert to user
+                    showCustomModal("Batch Successfully Added!", "success");
+                    //close the offcanvas sheet
+                    offCanvasExamAttemptCloseButton.click();
+                    //refresh the table
+                    refreshMarkTable();
+                    //refresh the form
+                    resetMarkForm();
+                } else {
+                    //this means there was a problem with the query
+                    //shows an error alert to the user
+                    showCustomModal("Operation Failed!" + serviceResponse, "error");
+                }
+            }
+                //will execute this block if the user confirmation is "no"
+            //show user an alert
+            else {
+                showCustomModal("Operation Cancelled!", "info");
+            }
+        });
+    } else {
+        //there are errors
+        //display them to the user using external-ModalFunction()
+        showCustomModal(errors, 'warning');
 
+    }
+}
+
+const checkMarkFormErrors = (object) => {
+    let errors = '';
+    if(object.batchID == null){
+        errors = errors + 'Batch is Required<br>';
+        $("#examMarksBatch_chosen .chosen-single").addClass('select-invalidated');
+        examMarksBatch.classList.add('is-invalid');
+    }
+
+    return errors;
 }
