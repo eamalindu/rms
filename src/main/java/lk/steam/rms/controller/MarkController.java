d215b0f20d4236b3f927fd3964b6a2b40ec76379
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-08-01 19:08:24 +0530

    edited

--- src/main/java/lk/steam/rms/controller/MarkController.java
+++ src/main/java/lk/steam/rms/controller/MarkController.java
@@ -123,4 +123,34 @@ public class MarkController {
         }
     }
 
+    @PutMapping()
+    public String updateMark(@RequestBody Mark mark) {
+        // Get the logged user's privilege
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        Privilege loggedUserPrivilege = privilegeController.getPrivilegeByUserAndModule(auth.getName(), "MARK");
+
+        // Check if the logged user has the privilege to verify a mark
+        if (!loggedUserPrivilege.getUpdatePrivilege()) {
+            return "<br>User does not have sufficient privilege.";
+        }
+
+        // Check if the mark already exists
+        Mark existMark = markDAO.getMarkByRegistrationIDAndLessonID(mark.getRegistrationID().getId(), mark.getLessonID().getId());
+
+        // If the mark does not exist, return an error message
+        if (existMark == null) {
+            return "<br>Mark does not exist.";
+        }
+
+        try {
+            markDAO.save(existMark);
+            return "OK";
+
+        }
+        // If an exception occurs, return an error message
+        catch (Exception ex) {
+            return "Verification Failed " + ex.getMessage();
+        }
+    }
+
 }
