bc7c0f64f648e84e7539440eff2a635faeb44565
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-08-01 14:16:22 +0530

    edited

--- src/main/resources/static/resources/js/controllerJS/exam.js
+++ src/main/resources/static/resources/js/controllerJS/exam.js
@@ -11,11 +11,12 @@ const refreshMarkTable = () => {
     //getting current Marks from the database using ajaxGetRequest function and assign the response to the variable batches
     marks = ajaxGetRequest("/Mark/findall");
     //creating a display property list for the Mark
-    displayPropertyListForMark = [{property: 'name', dataType: 'text'},
-        {property: 'code', dataType: 'text'},
-        {property: getDuration, dataType: 'function'},
-        {property: 'minimumRequirement', dataType: 'text'},
-        {property: 'lectureHours', dataType: 'text'},
+    displayPropertyListForMark = [
+        {property: getRegistrationNumber, dataType: 'function'},
+        {property: getStudent, dataType: 'function'},
+        {property: getBatch, dataType: 'function'},
+        {property: getLesson, dataType: 'function'},
+        {property: 'mark', dataType: 'text'},
         {property: getStatus, dataType: 'function'},]
     //using external function fillDataIntoTable to fill the data to the table tblExamMarks according to the displayPropertyListForMark list
     fillDataIntoTable(tblExamMarks, marks, displayPropertyListForMark, rowView, 'offcanvasMarkSheet');
@@ -25,5 +26,30 @@ const refreshMarkTable = () => {
         //initializing DataTable for the tblExamMarks table
         $('#tblExamMarks').DataTable();
     }
+
+}
+
+const getRegistrationNumber =(ob)=>{
+    return ob.registrationID.registrationNumber;
+}
+const getStudent =(ob)=>{
+    return ob.registrationID.studentID.nameWithInitials;
+}
+const getBatch =(ob)=>{
+    return ob.batchID.courseID.name + ' (' + ob.batchID.courseID.code + ')<br/><small class="text-muted">' + ob.batchID.batchCode + '</small>';
+}
+const getLesson =(ob)=>{
+    return ob.lessonID.name;
+}
+const getStatus =(ob)=>{
+    if(ob.isVerified) {
+        return '<span class="badge rounded-0" style="background: #3FB618">Verified</span>';
+    }
+    else{
+        return '<span class="badge rounded-0" style="background: #ea2f1e">Not-Verified</span>';
+    }
+}
+
+const rowView = (ob)=>{
 
 }
