288a104e75d9c592c81a9932d2667a8c20735a75
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-08-01 21:06:05 +0530

    edited

--- src/main/java/lk/steam/rms/controller/ReportController.java
+++ src/main/java/lk/steam/rms/controller/ReportController.java
@@ -193,5 +193,27 @@ public class ReportController {
         return reportView;
     }
 
+    @GetMapping(value = "/Mark-Report")
+    public ModelAndView markReportUI(){
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        ModelAndView reportView = new ModelAndView();
+        reportView.setViewName("attendanceReport.html");
+
+        reportView.addObject("username",auth.getName());
+        reportView.addObject("title","Attendance Report | STEAM RMS");
+        reportView.addObject("activeNavItem","attendance");
+
+        String loggedInEmployeeName = userDAO.getUserByUsername(auth.getName()).getEmployeeID().getFullName();
+        String loggedInDesignationName = userDAO.getUserByUsername(auth.getName()).getEmployeeID().getDesignationID().getDesignation();
+        byte[] photoBytes = userDAO.getUserByUsername(auth.getName()).getEmployeeID().getPhotoPath();
+        String base64Image = Base64.getEncoder().encodeToString(photoBytes);
+        String imageSrc = "data:image/png;base64," + base64Image;
+        reportView.addObject("loggedInEmployeeName",loggedInEmployeeName);
+        reportView.addObject("loggedInDesignationName",loggedInDesignationName);
+        reportView.addObject("loggedInImage",imageSrc);
+        return reportView;
+    }
+
 
 }
