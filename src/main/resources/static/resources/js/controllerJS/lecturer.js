4c56ac619c06be6285cfae8929ed6e98044af446
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-08-03 15:00:46 +0530

    edited

--- src/main/resources/static/resources/js/controllerJS/lecturer.js
+++ src/main/resources/static/resources/js/controllerJS/lecturer.js
@@ -14,6 +14,8 @@ const resetLecturerForm = () => {
     $("#lecturerEmployee_chosen .chosen-single").removeClass('select-invalidated');
     lecturerEmployee.classList.remove('is-valid');
     lecturerEmployee.classList.remove('is-invalid');
+    lecturerName.classList.remove('is-valid');
+    lecturerName.classList.remove('is-invalid');
 
     frmNewLecturer.reset();
     newLecturer = {};
@@ -61,4 +63,66 @@ const getStatus = (ob) => {
 
 const rowView=(ob)=>{
 
+}
+
+const newLecturerSubmit = ()=>{
+    console.log(newLecturer);
+    //calling the checkBatchFormErrors function and catching the return value to errors variable
+    let errors = checkLecturerFormErrors(newLecturer);
+    //check the errors variable is null
+    //if it's empty that means all the required inputs are filled
+    if (errors === '') {
+        //get a user confirmation using external customConfirm js
+        showCustomConfirm("You are about to add a New Lecturer<br>Are You Sure?", function (result) {
+            if (result) {
+                //if the user confirmation is "yes" call the ajaxHttpRequest to pass the data to backend via ajax
+                //catch the return value from the backend and save it in the serviceResponse variable
+                let serviceResponse = ajaxHttpRequest("/Lecturer", 'POST', newLecturer);
+                //check the serviceResponse value is "OK"
+                if (serviceResponse === "OK") {
+                    //this means data successfully passed to the backend
+                    //show an alert to user
+                    showCustomModal("Lecturer Successfully Added!", "success");
+                    //close the offcanvas sheet
+                    offCanvasLecturerCloseButton.click();
+                    //refresh the table
+                    refreshLecturerTable();
+                    //refresh the form
+                    resetLecturerForm();
+                } else {
+                    //this means there was a problem with the query
+                    //shows an error alert to the user
+                    showCustomModal("Operation Failed!" + serviceResponse, "error");
+                }
+            }
+                //will execute this block if the user confirmation is "no"
+            //show user an alert
+            else {
+                showCustomModal("Operation Cancelled!", "info");
+            }
+        });
+    } else {
+        //there are errors
+        //display them to the user using external-ModalFunction()
+        showCustomModal(errors, 'warning');
+
+    }
+}
+
+const checkLecturerFormErrors = (object)=>{
+    let errors ='';
+    if(object.employeeID==null){
+        errors +='Employee is Required<br>';
+        lecturerEmployee.classList.add('is-invalid');
+        $("#lecturerEmployee_chosen .chosen-single").addClass('select-invalidated');
+    }
+    if(object.name===null){
+        errors +='Name is Required<br>';
+        lecturerName.classList.add('is-invalid');
+    }
+    return errors;
+}
+
+const correctAssign =()=>{
+    newLecturer.employeeID = newLecturer.employeeID.id;
 }
