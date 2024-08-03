03b9212827bd4e61cf7f1b8b7058af4260923449
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-08-03 11:52:02 +0530

    edited

---
+++ src/main/resources/static/resources/js/controllerJS/payment.js
@@ -0,0 +1,37 @@
+window.addEventListener('load', () => {
+    resetSearchForm();
+    refreshPaymentTable();
+})
+
+const resetSearchForm = () => {
+    var start = moment().startOf('month');
+    var end = moment().endOf('month');
+
+    function cb(start, end) {
+        $('#registrationSearchDateRange span').html(start.format('YYYY-MMMM-DD') + ' - ' + end.format('YYYY-MMMM-DD'));
+    }
+
+    $('#registrationSearchDateRange').daterangepicker({
+        startDate: start, endDate: end,
+        locale: {
+            "format": "YYYY-MM-DD",
+        }, ranges: {
+            'Today': [moment(), moment()],
+            'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+            'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+            'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+            'This Month': [moment().startOf('month'), moment().endOf('month')],
+            'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')],
+            'Last Year': [moment().subtract(1, 'year').startOf('year'), moment().subtract(1, 'year').endOf('year')]
+        }
+    }, cb);
+
+    cb(start, end);
+
+}
+
+const refreshPaymentTable = ()=>{
+    [startDate,endDate] = registrationSearchDateRange.value.split(" - ")
+    const payments = ajaxGetRequest("/Payment/getPaymentsByStartDateAndEndDate/"+startDate+"/"+endDate);
+
+}
